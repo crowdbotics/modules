5f81649ee4e6e8a84695fb670f5361e2e74622ce
Daniel Sousa <daniel.s@crowdbotics.com>
2021-01-13 15:26:56 +0000

    Remove splash screen from demo app

--- demo/src/navigator/mainNavigator.js
+++ demo/src/navigator/mainNavigator.js
@@ -1,8 +1,6 @@
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import {createDrawerNavigator} from 'react-navigation-drawer';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 
-import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 
@@ -12,12 +10,9 @@ import SideMenu from './sideMenu';
 
 const AppNavigator = {
 
-    //@BlueprintNavigationInsertion
+  //@BlueprintNavigationInsertion
 
-    /** new navigators can be added here */
-    SplashScreen: {
-      screen: SplashScreen
-    }
+  /** new navigators can be added here */
 };
 
 const DrawerAppNavigator = createDrawerNavigator(
