dbe9d36d5d062ea9f5fe3ef98558f30664399ecf
Daniel Sousa <daniel.s@crowdbotics.com>
2021-01-14 16:14:11 +0000

    Refactor splash into functional component

--- react-native/splash/index.js
+++ react-native/splash/index.js
@@ -1,45 +1,39 @@
-import React, { Component } from "react";
-import { Text, ScrollView, TouchableOpacity } from "react-native";
+import React from "react";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { styles } from './styles'
 
-export default class App extends Component {
-  static navigationOptions = {
-    title: 'Installed blueprints',
-  };
+export default function Splash(props) {
+  const { installed_blueprints, navigation: { navigate } } = props;
 
-  renderItems() {
-    const {
-      navigation: { navigate },
-    } = this.props;
-
-    const installed_blueprints = [];
-
+  if (installed_blueprints) {
     return installed_blueprints.map(item => {
       if (item.hasOwnProperty('access_route')) {
         return (
-          <TouchableOpacity
-            onPress={_ => navigate(item.access_route)}
-            style={styles.item}
-            key={`${item.name}--blueprint-button`}>
-            <Icon
-              style={styles.itemLogo}
-              name={item.icon_name ? item.icon_name : 'pencil-square-o'}
-              size={40}
-              color="#F88087"
-            />
-            <Text style={styles.itemFont}>{item.human_name}</Text>
-          </TouchableOpacity>
+          <ScrollView contentContainerStyle={styles.itemsContainer}>
+            <TouchableOpacity
+              onPress={_ => navigate(item.access_route)}
+              style={styles.item}
+              key={`${item.name}--blueprint-button`}>
+              <Icon
+                style={styles.itemLogo}
+                name={item.icon_name ? item.icon_name : 'pencil-square-o'}
+                size={40}
+                color="#F88087"
+              />
+              <Text style={styles.itemFont}>{item.human_name}</Text>
+            </TouchableOpacity>
+          </ScrollView>
         );
       }
     });
-  }
-
-  render() {
+  } else {
     return (
       <ScrollView contentContainerStyle={styles.itemsContainer}>
-        {this.renderItems()}
+        <View style={styles.empty}>
+          <Text style={styles.emptyText}>No modules installed.</Text>
+        </View>
       </ScrollView>
-    );
+    )
   }
 }
