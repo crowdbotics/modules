5bb753f1d70858cc3749b572f73dc8ecc52e2168
Daniel Sousa <daniel.s@crowdbotics.com>
2021-01-15 15:46:50 +0000

    Remove utils dependency from maps

--- react-native/maps/styles.js
+++ react-native/maps/styles.js
@@ -1,6 +1,16 @@
 import { StyleSheet } from 'react-native'
 
-import { scale, scaleVertical } from '../../utils/scale'
+import { Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+// Guideline sizes are based on standard ~5" screen mobile device
+const guidelineBaseWidth = 350;
+const guidelineBaseHeight = 680;
+
+const scale = size => (width / guidelineBaseWidth) * size;
+const scaleVertical = size => (height / guidelineBaseHeight) * size;
+const scaleModerate = (size, factor = 0.5) => size + ((scale(size) - size) * factor);
 
 export const styles = StyleSheet.create({
   view: {
