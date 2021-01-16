ec0e1213e907282ea05b7b4f02e9ddb9717e6cf4
Daniel Sousa <daniel.s@crowdbotics.com>
2021-01-16 17:26:16 +0000

    scripts: implement x-dependencies installation

--- README.md
+++ README.md
@@ -23,3 +23,19 @@ In our `crowdbotics-slack-app` backend we support two flags on each file:
 - [User Profile](react-native/user-profile)
 - [Articles](react-native/articles)
 - [Maps](react-native/maps)
+
+# Gotchas
+
+## x-dependencies
+
+React Native autolinking doesn't work with sub-dependencies.
+
+Example, including `react-native-maps` in the maps module will not link the native
+modules properly and results in
+
+```
+[Sat Jan 16 2021 16:23:45.446]  ERROR    Invariant Violation: requireNativeComponent: "AIRMap" was not found in the UIManager.
+```
+
+https://github.com/react-native-community/cli/issues/1347
+https://github.com/react-native-maps/react-native-maps/issues/3427#issuecomment-627135604
--- scripts/demo-remove.js
+++ scripts/demo-remove.js
@@ -1,11 +1,28 @@
+import fs from "fs";
 import path from "path";
 import { execShellCommand } from "./utils.js";
 
 const module = process.argv.slice(2)[0];
 const demoDir = path.join(process.cwd(), "demo");
 
+const originModuleDir = path.join(process.cwd(), "react-native", module);
 const targetModuleDir = path.join(demoDir, "src", "modules", module);
 const moduleName = `@modules/${module}`;
 
+let packages = [moduleName];
+
+// Remove x-dependencies
+const packageJSON = JSON.parse(
+  fs.readFileSync(path.join(originModuleDir, "package.json"))
+);
+if (packageJSON.hasOwnProperty("x-dependencies")) {
+  const deps = packageJSON["x-dependencies"];
+  for (const [key, _] of Object.entries(deps)) {
+    packages.push(`${key}`);
+  }
+}
+
+// Remove packages
+packages = packages.join(" ");
+execShellCommand(`cd ${demoDir} && yarn remove ${packages}`);
 execShellCommand(`rm -rf ${targetModuleDir}`)
-execShellCommand(`cd ${demoDir} && yarn remove ${moduleName}`);
