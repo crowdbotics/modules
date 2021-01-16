ad5bf69b38de71c02abfd854b097c80016cddbce
Daniel Sousa <daniel.s@crowdbotics.com>
2021-01-16 17:59:18 +0000

    template: refactor metro config

--- template/source/metro.config.js
+++ template/source/metro.config.js
@@ -7,7 +7,7 @@
 
 const path = require('path');
 const extraNodeModules = {
-  '@modules': path.resolve(__dirname + '/src/modules'),
+  '@modules': path.resolve(__dirname, 'src', 'modules'),
 };
 const watchFolders = [
   path.resolve(__dirname + '/src/modules')
@@ -25,7 +25,7 @@ module.exports = {
     extraNodeModules: new Proxy(extraNodeModules, {
       get: (target, name) =>
         //redirects dependencies referenced from src/modules to local node_modules
-        name in target ? target[name] : path.join(process.cwd(), `node_modules/${name}`),
+        name in target ? target[name] : path.join(process.cwd(), "node_modules", name),
     }),
   },
   watchFolders,
