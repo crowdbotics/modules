d486c8e44fca1ff2892826c170af2f7999c0e67f
Daniel Sousa <daniel.s@crowdbotics.com>
2021-01-18 16:16:18 +0000

    template: Fix typo

--- template/source/src/modules/index.js
+++ template/source/src/modules/index.js
@@ -2,5 +2,5 @@ import { modules } from "./manifest.js";
 import { getPropertyMap } from "./utils.js";
 
 export const reducers = getPropertyMap(modules, "reducer");
-export const reducers = getPropertyMap(modules, "actions");
+export const actions = getPropertyMap(modules, "actions");
 export default getPropertyMap(modules, "screen");
