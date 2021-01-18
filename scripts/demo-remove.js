6076348010afdce765071e2a6cd1de372c860a20
Daniel Sousa <daniel.s@crowdbotics.com>
2021-01-18 15:16:33 +0000

    modules: add/remove module to manifest.js

--- scripts/demo-remove.js
+++ scripts/demo-remove.js
@@ -1,19 +1,26 @@
-import fs from "fs";
 import path from "path";
-import { execShellCommand } from "./utils.js";
+import {
+  ManifestTransformer,
+  execShellCommand,
+  read,
+  generate,
+  pretty,
+  write,
+  parse
+} from "./utils.js";
 
 const module = process.argv.slice(2)[0];
 const demoDir = path.join(process.cwd(), "demo");
 
 const originModuleDir = path.join(process.cwd(), "react-native", module);
-const targetModuleDir = path.join(demoDir, "src", "modules", module);
+const targetModuleDir = path.join(demoDir, "src", "modules");
 const moduleName = `@modules/${module}`;
 
 let packages = [moduleName];
 
 // Remove x-dependencies
 const packageJSON = JSON.parse(
-  fs.readFileSync(path.join(originModuleDir, "package.json"))
+  read(path.join(originModuleDir, "package.json"))
 );
 if (packageJSON.hasOwnProperty("x-dependencies")) {
   const deps = packageJSON["x-dependencies"];
@@ -25,4 +32,14 @@ if (packageJSON.hasOwnProperty("x-dependencies")) {
 // Remove packages
 packages = packages.join(" ");
 execShellCommand(`cd ${demoDir} && yarn remove ${packages}`);
-execShellCommand(`rm -rf ${targetModuleDir}`)
+const rmDir = path.join(targetModuleDir, module);
+execShellCommand(`rm -rf ${rmDir}`)
+
+// Update manifest
+const manifest = path.join(targetModuleDir, "manifest.js");
+let code = read(manifest);
+const transformer = new ManifestTransformer({ add: false, module: module });
+let node = parse(code);
+node = transformer.visit(node);
+code = pretty(generate(node));
+write(manifest, code);
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,5 +1,10 @@
 import fs from "fs";
 import path from "path";
+import prettier from "prettier";
+import types from "@babel/types";
+import traverse from "@babel/traverse";
+import parser from "@babel/parser";
+import generator from "@babel/generator";
 import child_process from "child_process";
 
 export function clean({ target }) {
@@ -19,4 +24,87 @@ export function execShellCommand(cmd) {
       resolve(stdout ? true : false);
     });
   });
+}
+
+export const read = path => {
+  return fs.readFileSync(path, "utf8");
+};
+
+export const write = (path, data) => {
+  return fs.writeFileSync(path, data);
+}
+
+export const pretty = code => {
+  return prettier.format(
+    code,
+    {
+      semi: false,
+      parser: "babel",
+      trailingComma: "none",
+      arrowParens: "avoid"
+    }
+  );
+};
+
+export const generate = node => {
+  return generator.default(node, {
+    retainLines: true,
+    compact: false
+  }).code;
+};
+
+export const parse = code => {
+  return parser.parse(code, {
+    sourceType: "module",
+    plugins: ["jsx", "classProperties"]
+  });
+};
+
+export class ManifestTransformer {
+  constructor({ add, module }) {
+    this.add = add;
+    this.module = module;
+    this.capitalizedModule = module.trim().replace(/^\w/, (c) => c.toUpperCase());
+  }
+
+  visit(node) {
+    if (this.add) {
+      traverse.default(node, {
+        // Push array element
+        ArrayExpression: path => {
+          let elements = path.node.elements;
+          elements.push(types.identifier(this.capitalizedModule));
+          path.replaceWith(types.arrayExpression(elements));
+          path.skip();
+        }
+      });
+      // Push Import
+      node.program.body = [
+        types.importDeclaration(
+          [
+            types.importDefaultSpecifier(types.identifier(this.capitalizedModule))
+          ],
+          types.stringLiteral(`./${this.module}`)
+        ),
+        ...node.program.body
+      ]
+    } else {
+      traverse.default(node, {
+        // Filter array element
+        ArrayExpression: path => {
+          path.replaceWith(types.arrayExpression(
+            path.node.elements.filter(ele => ele.name != this.capitalizedModule)
+          ));
+          path.skip();
+        },
+        // Filter import
+        ImportDeclaration: path => {
+          if (path.node.source.value == `./${this.module}`) {
+            path.remove();
+          };
+        }
+      });
+    }
+    return node;
+  }
 }
