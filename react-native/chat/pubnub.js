2fe18c134855ef88a3972ad3792444e3725e5b87
Daniel Sousa <daniel.s@crowdbotics.com>
2021-01-19 18:28:49 +0000

    modules: chat - Rename pubnub component file

--- react-native/chat/pubnub.js
+++ react-native/chat/pubnub.js
@@ -4,9 +4,6 @@ import PubNubReact from 'pubnub-react';
 import uuid from 'uuid'
 
 export const appConfig = {
-  // todo add library to handle env variables
-  emailAuthAPIEndPoint: "https://modules-dev-18197.botics.co",
-  defaultTimeout: 5000,
   pubPublishKey: 'pub-c-09038c51-029c-47a7-95f2-b6c7407afb12',
   pubSubscribeKey: 'sub-c-6b1bee04-ba9f-11e9-8753-ce76e7dc5905'
 };
