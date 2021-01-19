e39eb527aaa9e8db1e67c3d6811e98b0ee9eccc8
Daniel Sousa <daniel.s@crowdbotics.com>
2021-01-19 19:25:06 +0000

    template: add container styles to main view

--- template/source/src/config/index.js
+++ template/source/src/config/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux'
 import store from './store.js';
 
@@ -8,12 +8,19 @@ import modules from "@modules";
 const App = () => {
   return (
     <Provider store={store}>
-      <View>
+      <View style={styles.container}>
         <Text>Total modules installed: {Object.keys(modules).length}</Text>
         <Text>Modules: {Object.keys(modules)}</Text>
       </View>
     </Provider>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 100
+  }
+})
 
 export default App;
