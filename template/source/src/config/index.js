6a61c268a8ac3346f383e88ae6560a4db5c132da
Daniel Sousa <daniel.s@crowdbotics.com>
2021-01-21 21:55:46 +0000

    template: add navigation setup

--- template/source/src/config/index.js
+++ template/source/src/config/index.js
@@ -1,17 +1,50 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Button, View, Text, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux'
 import store from './store.js';
 
 import modules from "@modules";
 
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function Welcome({ navigation }) {
+  const links = modules.map(module => {
+    return (
+      <View style={styles.button}>
+        <Button
+          key={module.screen}
+          title={module.name}
+          onPress={() => navigation.navigate(module.name)} />
+      </View>
+    )
+  });
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Total modules installed: {modules.length}</Text>
+      <Text style={styles.text}>Screens available:</Text>
+      {links}
+    </View>
+  )
+}
+
 const App = () => {
+  const modulesRoutes = modules.map(module => {
+    return (
+      <Stack.Screen key={module} name={module.name} component={module.screen} />
+    )
+  })
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Total modules installed: {Object.keys(modules).length}</Text>
-        <Text>Modules: {Object.keys(modules)}</Text>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Welcome">
+          <Stack.Screen name="Welcome" component={Welcome} />
+          {modulesRoutes}
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 };
@@ -19,7 +52,14 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: 100
+    height: 100,
+    padding: 10
+  },
+  text: {
+    fontSize: 20
+  },
+  button: {
+    marginTop: 10
   }
 })
 
--- template/source/src/modules/index.js
+++ template/source/src/modules/index.js
@@ -3,4 +3,5 @@ import { getPropertyMap } from "./utils.js";
 
 export const reducers = getPropertyMap(modules, "reducer");
 export const actions = getPropertyMap(modules, "actions");
-export default getPropertyMap(modules, "screen");
+export const screens = getPropertyMap(modules, "screen");
+export default modules;
