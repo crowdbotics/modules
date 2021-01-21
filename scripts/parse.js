f3f57c105fa2b2d84da12feec67f672e25c21f21
Daniel Sousa <daniel.s@crowdbotics.com>
2021-01-21 21:58:17 +0000

    modules: parse django modules

--- scripts/parse.js
+++ scripts/parse.js
@@ -1,11 +1,12 @@
 import fs from "fs";
 import path from "path";
 
+let data;
+const DJANGO_MODULES = path.join("django");
+const DJANGO_OUTPUT_FILE = "django.json";
 const REACT_NATIVE_MODULES = path.join("react-native");
-const REACT_NATIVE_OUTPUT_FILE = "data.json";
-const ACCEPTED_EXTENSIONS = [".json", ".js", ".ts", ".jsx", ".tsx", ".md"];
-
-let data = {};
+const REACT_NATIVE_OUTPUT_FILE = "react-native.json";
+const ACCEPTED_EXTENSIONS = [".json", ".js", ".ts", ".jsx", ".tsx", ".md", ".py"];
 
 const parseDir = dir => {
   let entries = fs.readdirSync(dir);
@@ -29,7 +30,7 @@ const parseModules = dir => {
   })
 }
 
-const parseData = (data, file) => {
+const parseData = ({ data, file, parentDir = null, newFile = true }) => {
   let map = {};
   Object.keys(data).map(key => {
     let paths = key.split("/");
@@ -41,13 +42,17 @@ const parseData = (data, file) => {
     Object.assign(map[base[1]], {
       [file]: {
         code: data[key],
-        parentDir: null,
-        newFile: true
+        parentDir: parentDir,
+        newFile: newFile
       }
     });
   })
   fs.writeFileSync(file, JSON.stringify(map, null, 2));
 }
 
+data = {};
 parseModules(REACT_NATIVE_MODULES);
-parseData(data, REACT_NATIVE_OUTPUT_FILE);
+parseData({ data: data, file: REACT_NATIVE_OUTPUT_FILE });
+data = {};
+parseModules(DJANGO_MODULES);
+parseData({ data: data, file: DJANGO_OUTPUT_FILE, parentDir: "/" });
