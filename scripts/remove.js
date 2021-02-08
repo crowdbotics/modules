d52a49c25e5caaf12cec4f804ce3592ff8b91c01
Daniel Sousa <daniel.s@crowdbotics.com>
2021-02-08 16:05:19 +0000

    scripts: support add/remove multiple modules

--- scripts/remove.js
+++ scripts/remove.js
@@ -9,39 +9,42 @@ import {
   parse
 } from "./utils.js";
 
-const module = process.argv.slice(2)[0];
+const modules = process.argv.slice(2)[0];
+const cwd = process.cwd();
 const demoDir = path.join(process.cwd(), "demo");
+modules.map(module => {
+  process.chdir(cwd);
+  const originModuleDir = path.join(process.cwd(), "react-native", module);
+  const targetModuleDir = path.join(demoDir, "src", "modules");
+  const moduleName = `@modules/${module}`;
 
-const originModuleDir = path.join(process.cwd(), "react-native", module);
-const targetModuleDir = path.join(demoDir, "src", "modules");
-const moduleName = `@modules/${module}`;
+  let packages = [moduleName];
 
-let packages = [moduleName];
-
-// Remove x-dependencies
-const packageJSON = JSON.parse(
-  read(path.join(originModuleDir, "package.json"))
-);
-if (packageJSON.hasOwnProperty("x-dependencies")) {
-  const deps = packageJSON["x-dependencies"];
-  // TODO: Do we want to remove blindly? What if the user installed that dep too?
-  for (const [key, _] of Object.entries(deps)) {
-    packages.push(`${key}`);
+  // Remove x-dependencies
+  const packageJSON = JSON.parse(
+    read(path.join(originModuleDir, "package.json"))
+  );
+  if (packageJSON.hasOwnProperty("x-dependencies")) {
+    const deps = packageJSON["x-dependencies"];
+    // TODO: Do we want to remove blindly? What if the user installed that dep too?
+    for (const [key, _] of Object.entries(deps)) {
+      packages.push(`${key}`);
+    }
   }
-}
 
-// Remove packages
-packages = packages.join(" ");
-// TODO -- add process.chdir
-execShellCommand(`cd ${demoDir} && yarn remove ${packages}`);
-const rmDir = path.join(targetModuleDir, module);
-execShellCommand(`rm -rf ${rmDir}`)
+  // Remove packages
+  packages = packages.join(" ");
+  process.chdir(demoDir);
+  execShellCommand(`yarn remove ${packages}`);
+  const rmDir = path.join(targetModuleDir, module);
+  execShellCommand(`rm -rf ${rmDir}`)
 
-// Update manifest
-const manifest = path.join(targetModuleDir, "manifest.js");
-let code = read(manifest);
-const transformer = new ManifestTransformer({ add: false, module: module });
-let node = parse(code);
-node = transformer.visit(node);
-code = pretty(generate(node));
-write(manifest, code);
+  // Update manifest
+  const manifest = path.join(targetModuleDir, "manifest.js");
+  let code = read(manifest);
+  const transformer = new ManifestTransformer({ add: false, module: module });
+  let node = parse(code);
+  node = transformer.visit(node);
+  code = pretty(generate(node));
+  write(manifest, code);
+});
