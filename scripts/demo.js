53afe91db1e6a74a1846b7d6432700dbab3523b7
Daniel Sousa <daniel.s@crowdbotics.com>
2021-02-08 14:57:35 +0000

    scripts: yarn install after init

--- scripts/demo.js
+++ scripts/demo.js
@@ -5,4 +5,9 @@ const APP_NAME = "demo";
 const template = path.join("file:/", process.cwd(), "template");
 
 clean({ target: APP_NAME });
-execShellCommand(`npx react-native init ${APP_NAME} --template ${template}`);
+execShellCommand(
+  `npx react-native init ${APP_NAME} --template ${template}`
+).then(() => {
+  process.chdir(path.join(process.cwd(), APP_NAME));
+  execShellCommand("yarn install");
+});
