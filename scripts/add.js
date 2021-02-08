88cc92daf8ad36c1f1a314cc94c87f466ed5569c
Daniel Sousa <daniel.s@crowdbotics.com>
2021-02-08 16:05:19 +0000

    scripts: support add/remove multiple modules

--- scripts/add.js
+++ scripts/add.js
@@ -9,37 +9,40 @@ import {
   parse
 } from "./utils.js";
 
-const module = process.argv.slice(2)[0];
+const modules = process.argv.slice(2);
+const cwd = process.cwd();
 const demoDir = path.join(process.cwd(), "demo");
+modules.map(module => {
+  process.chdir(cwd);
+  const originModuleDir = path.join(process.cwd(), "react-native", module);
+  const targetModuleDir = path.join(demoDir, "src", "modules");
+  const yarnPath = path.join("file:./src", "modules", module);
 
-const originModuleDir = path.join(process.cwd(), "react-native", module);
-const targetModuleDir = path.join(demoDir, "src", "modules");
-const yarnPath = path.join("file:./src", "modules", module);
+  let packages = [yarnPath];
+  execShellCommand(`cp -r ${originModuleDir} ${targetModuleDir}`);
 
-let packages = [yarnPath];
-execShellCommand(`cp -r ${originModuleDir} ${targetModuleDir}`);
-
-// Install x-dependencies
-const packageJSON = JSON.parse(
-  read(path.join(originModuleDir, "package.json"))
-);
-if (packageJSON.hasOwnProperty("x-dependencies")) {
-  const deps = packageJSON["x-dependencies"];
-  for (const [key, value] of Object.entries(deps)) {
-    packages.push(`${key}@${value}`);
+  // Install x-dependencies
+  const packageJSON = JSON.parse(
+    read(path.join(originModuleDir, "package.json"))
+  );
+  if (packageJSON.hasOwnProperty("x-dependencies")) {
+    const deps = packageJSON["x-dependencies"];
+    for (const [key, value] of Object.entries(deps)) {
+      packages.push(`${key}@${value}`);
+    }
   }
-}
 
-// Install packages
-packages = packages.join(" ");
-// TODO -- add process.chdir
-execShellCommand(`cd ${demoDir} && yarn add ${packages}`);
+  // Install packages
+  packages = packages.join(" ");
+  process.chdir(demoDir);
+  execShellCommand(`cd ${demoDir} && yarn add ${packages}`);
 
-// Update manifest
-const manifest = path.join(targetModuleDir, "manifest.js");
-let code = read(manifest);
-const transformer = new ManifestTransformer({ add: true, module: module });
-let node = parse(code);
-node = transformer.visit(node);
-code = pretty(generate(node));
-write(manifest, code);
+  // Update manifest
+  const manifest = path.join(targetModuleDir, "manifest.js");
+  let code = read(manifest);
+  const transformer = new ManifestTransformer({ add: true, module: module });
+  let node = parse(code);
+  node = transformer.visit(node);
+  code = pretty(generate(node));
+  write(manifest, code);
+});
