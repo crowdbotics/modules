7c3f4e6de8050b5f1a03fb84fb2a9a80fcb08f17
Daniel Sousa <daniel.s@crowdbotics.com>
2021-02-08 18:40:40 +0000

    template: sort modules by navigator and menu first

--- template/source/src/modules/index.js
+++ template/source/src/modules/index.js
@@ -27,14 +27,32 @@ const YourAppModule = {
   navigator: YourApp
 }
 
-const validate = mod => {
-  return mod.hasOwnProperty("title");
+const sortNavigators = (a, b) => {
+  if (a.hasOwnProperty("navigator") && b.hasOwnProperty("navigator")) {
+    return 0;
+  } else if (a.hasOwnProperty("navigator")) {
+    return -1;
+  } else {
+    return 1;
+  }
+}
+
+const sortMenu = (a, b) => {
+  if (a.title == "App Menu") {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+const validate = (mod, prop) => {
+  return mod.hasOwnProperty(prop);
 };
 
 const getModules = () => {
   // normalize modules
   let modules = manifest.map(mod => {
-    if (validate(mod)) {
+    if (validate(mod, "title")) {
       return mod;
     } else {
       return {
@@ -43,7 +61,12 @@ const getModules = () => {
       }
     }
   });
-  return modules.length ? modules : [YourAppModule];
+  modules = modules.sort(sortNavigators);
+  modules = modules.sort(sortMenu);
+  if (!(modules.length && modules[0].hasOwnProperty("navigator"))) {
+    modules.splice(0, 0, YourAppModule);
+  }
+  return modules;
 }
 
 export const slices = Object.entries(
