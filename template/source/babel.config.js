f6b9c11fbdd93be7f010b705aad026f56bf3e85a
Daniel Sousa <daniel.s@crowdbotics.com>
2021-02-08 17:10:36 +0000

    template: add react-native-dotenv config and template

--- template/source/babel.config.js
+++ template/source/babel.config.js
@@ -3,6 +3,13 @@ module.exports = {
     'module:metro-react-native-babel-preset'
   ],
   plugins: [
-    'module:react-native-dotenv'
+    ["module:react-native-dotenv", {
+      "moduleName": "@env",
+      "path": ".env",
+      "blacklist": null,
+      "whitelist": null,
+      "safe": false,
+      "allowUndefined": true
+    }]
   ]
 };
