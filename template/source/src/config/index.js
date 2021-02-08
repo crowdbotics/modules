852184a95981c2a2bae1d78c2f34de9c28bde89a
Daniel Sousa <daniel.s@crowdbotics.com>
2021-02-08 18:41:18 +0000

    template: root component inject hooks

--- template/source/src/config/index.js
+++ template/source/src/config/index.js
@@ -2,8 +2,14 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./store";
 import Navigation from "./navigation";
+import { hooks } from "@modules";
 
 const App = () => {
+  let effects = {};
+  hooks.map(([_, hook]) => {
+    effects[hook.name] = hook();
+  });
+
   return (
     <Provider store={store}>
       <Navigation />
