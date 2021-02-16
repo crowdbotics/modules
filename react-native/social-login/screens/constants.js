7d3c484bca19eaa30c16a8a7c1b9de61d2cfed2e
Aline Tavares <alaisgomes@gmail.com>
2021-02-16 12:26:23 -0500

    Update react-native module

--- react-native/social-login/screens/constants.js
+++ react-native/social-login/screens/constants.js
@@ -3,4 +3,4 @@ export const BACKGROUND_URL = "https://crowdbotics-slack-dev.s3.amazonaws.com/me
 export const validateEmail = /^[^\s]+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 // Change here the  name of the screen you want to redirect after Login
-export const HOME_SCREEN_NAME = "BlankScreen117194"
+export const HOME_SCREEN_NAME = "BlankScreen117459"
