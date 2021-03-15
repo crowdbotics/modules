d2a6333dd0a6792321b9845144b5626ea6673627
Aline Tavares <alaisgomes@gmail.com>
2021-03-15 15:45:56 -0400

    Update splash and t&c

--- react-native/splash/index.js
+++ react-native/splash/index.js
@@ -1,44 +1,32 @@
-import React from "react";
-import {
-  View,
-  Image,
-  ImageBackground,
-  TouchableOpacity,
-  Text,
-  Button,
-  Switch,
-  TextInput,
-  StyleSheet,
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import { SlideMenuIcon } from '../../navigator/slideMenuIcon';
+import React, { useEffect } from 'react';
+import { View, StyleSheet, Image } from 'react-native';
 
-export default class Blank extends React.Component {
-  componentDidMount() {
+const NEXT_SCREEN_NAME = 'TermsAndConditions';
+
+
+const Splash = ({ navigation }) => {
+  useEffect(() => {
     setTimeout(() => {
-      this.props.navigation.navigate('Onboarding177768') //onboarding
-    }, 3000);
-  }
-  static navigationOptions = ({ navigation }) => {
-    return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
+      navigation.navigate(NEXT_SCREEN_NAME);
+    }, 3000)
+  }, []);
 
-  state = {};
-
-  render = () => (
+  return (
     <View style={styles.container}>
-      <Image style={{ width: '100%', height: '100%' }} source={require('../../assets/images/splashBack.png')} />
+      <Image style={styles.image} source={require('./assets/splashBack.png')} />
     </View>
   );
-}
+  }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#FFF',
   },
+  image: { width: '100%' },
 });
+
+export default {
+  title: 'SplashScreen',
+  navigator: Splash
+}
