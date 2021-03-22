8a0221992ec6a7abfc2b5d4f3fcecb282bb10a4e
Daniel <daniel.s@crowdbotics.com>
2021-03-22 18:21:10 +0000

    install dependencies after using template

--- scaffold/install.js
+++ scaffold/install.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
-
 const cwd = process.cwd();
 const customFiles = path.join(cwd, "custom");
 
@@ -19,9 +18,16 @@ fs.copyFileSync(path.join(customFiles, "metro.config.js"), path.join(cwd, "metro
 fs.copyFileSync(path.join(customFiles, ".env"), path.join(cwd, ".env"));
 fs.copyFileSync(path.join(customFiles, ".env.template"), path.join(cwd, ".env.template"));
 
-// Cleanup
-fs.rmdirSync(path.join(customFiles), { recursive: true });
-
 // react-native cli bug?
 // https://github.com/react-native-community/cli/blob/641b21f583c97e3d48ce87d5fe804f42db92fa5c/packages/cli/src/tools/generator/copyProjectTemplateAndReplace.ts#L144
 fs.renameSync(path.join(cwd, "_editorconfig"), path.join(cwd, ".editorconfig"))
+
+// package.json manipulation
+const packageFile = path.join(cwd, "package.json");
+const packageJson = require(packageFile);
+const dependencies = require(path.join(customFiles, "dependencies.json"));
+packageJson.dependencies = Object.assign(packageJson.dependencies, dependencies);
+fs.writeFileSync(packageFile, JSON.stringify(packageJson, null, 2));
+
+// Cleanup
+fs.rmdirSync(path.join(customFiles), { recursive: true });
