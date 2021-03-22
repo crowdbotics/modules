e8b9daec8efde444db70cb15d3ee4e116df32503
Daniel <daniel.s@crowdbotics.com>
2021-03-22 17:49:17 +0000

    first version of postinit script

--- scaffold/install.js
+++ scaffold/install.js
@@ -1,1 +1,27 @@
 #!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
+const cwd = process.cwd();
+const customFiles = path.join(cwd, "custom");
+
+// Crowdbotics src files
+fs.renameSync(path.join(customFiles, "src"), path.join(cwd, "src"));
+
+// CircleCI
+fs.renameSync(path.join(customFiles, ".circleci"), path.join(cwd, ".circleci"));
+
+// File overrides
+fs.copyFileSync(path.join(customFiles, "App.js"), path.join(cwd, "App.js"));
+fs.copyFileSync(path.join(customFiles, "babel.config.js"), path.join(cwd, "babel.config.js"));
+fs.copyFileSync(path.join(customFiles, "heroku.yml"), path.join(cwd, "heroku.yml"));
+fs.copyFileSync(path.join(customFiles, "metro.config.js"), path.join(cwd, "metro.config.js"));
+fs.copyFileSync(path.join(customFiles, ".env"), path.join(cwd, ".env"));
+fs.copyFileSync(path.join(customFiles, ".env.template"), path.join(cwd, ".env.template"));
+
+// Cleanup
+fs.rmdirSync(path.join(customFiles), { recursive: true });
+
+// react-native cli bug?
+// https://github.com/react-native-community/cli/blob/641b21f583c97e3d48ce87d5fe804f42db92fa5c/packages/cli/src/tools/generator/copyProjectTemplateAndReplace.ts#L144
+fs.renameSync(path.join(cwd, "_editorconfig"), path.join(cwd, ".editorconfig"))
