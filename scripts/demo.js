28667fc9351afd21e1331735f02d3a7b597abb47
Daniel <daniel.s@crowdbotics.com>
2021-03-22 20:12:05 +0000

    scripts: use config on demo.js

--- scripts/demo.js
+++ scripts/demo.js
@@ -2,10 +2,9 @@ import path from "path";
 import { clean, execShellCommand } from "./utils.js";
 import config from "../config.js";
 
-const template = path.join("file:/", process.cwd(), "scaffold");
+const template = path.join("file:/", process.cwd(), config.scaffold.directory);
 
 clean({ target: path.join(process.cwd(), config.demo.directory) });
-
 execShellCommand(
-  `npx react-native init ${config.demo.name} --template ${template} --version ${config.versions.rn}`
+  `npx react-native init ${config.demo.placeholderName} --template ${template} --version ${config.versions.rn}`
 );
