421615bc7238a7095689a6f7565deb66ab589a8e
Daniel <daniel.s@crowdbotics.com>
2021-03-22 17:09:19 +0000

    use version and new scaffold on demo.js

--- scripts/demo.js
+++ scripts/demo.js
@@ -2,11 +2,12 @@ import path from "path";
 import { clean, execShellCommand } from "./utils.js";
 
 const APP_NAME = "demo";
-const template = path.join("file:/", process.cwd(), "template");
+const version = "0.64.0";
+const template = path.join("file:/", process.cwd(), "scaffold");
 
 clean({ target: APP_NAME });
 execShellCommand(
-  `npx react-native init ${APP_NAME} --template ${template}`
+  `npx react-native init ${APP_NAME} --template ${template} --version ${version}`
 ).then(() => {
   process.chdir(path.join(process.cwd(), APP_NAME));
   execShellCommand("yarn install");
