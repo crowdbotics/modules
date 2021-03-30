372878119e44bceb41de5c260590501816e85e27
Daniel <daniel.s@crowdbotics.com>
2021-03-30 16:47:08 +0100

    modules: articles - update navigator to v5

--- modules/react-native/articles/navigator.js
+++ modules/react-native/articles/navigator.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import { SlideMenuIcon } from '../../navigator/slideMenuIcon';
-import { createStackNavigator } from "react-navigation-stack";
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
 
 import Articles from "./index";
 import Article from "./article";
 
-export default ArticlesNavigator = createStackNavigator({
-  Articles: {
-    screen: Articles.screen,
-    navigationOptions: ({ navigation }) => ({
-      title: "Article List",
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    }),
-    params: {
-      detail: "Article"
-    }
-  },
-  Article: {
-    screen: Article,
-  },
-});
+export const ArticlesNavigator = () => {
+  return (
+    <Stack.Navigator headerMode="none" initialRouteName="Articles">
+      <Stack.Screen name="Articles" component={Articles} />
+      <Stack.Screen name="Article" component={Article} />
+    </Stack.Navigator>
+  )
+}
