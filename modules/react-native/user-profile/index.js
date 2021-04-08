92875e3acd8c5d6d1f4768f1045ee7e7f323fed7
Aline Tavares <alaisgomes@gmail.com>
2021-04-08 16:13:59 -0400

    Update user profile

--- modules/react-native/user-profile/index.js
+++ modules/react-native/user-profile/index.js
@@ -1,62 +1,58 @@
-import React, { Component } from 'react';
-import { View, ScrollView, ActivityIndicator, Text, Button } from 'react-native';
-import { user_read, user_update } from './store/actions';
-import { styles, Color } from './styles';
-import { connect } from 'react-redux';
-import reducer from './store/reducers';
-import EditUser from './edit';
-import ViewUser from './view';
+import React, { Component } from "react"
+import { View, ScrollView, ActivityIndicator, Text, Button } from "react-native"
+import { user_read, user_update } from "./store/actions"
+import { styles, Color } from "./styles"
+import { connect } from "react-redux"
+import reducer from "./store/reducers"
+import EditUser from "./edit"
+import ViewUser from "./view"
 
 export class UserDetail extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      loading: false,
-    };
+      loading: false
+    }
   }
 
   load() {
-    const { route } = this.props;
-    const { auth_user } = this.props;
-    const id = route.params?.id || auth_user.id;
+    const { route } = this.props
+    const { auth_user } = this.props
+    const id = route.params?.id || auth_user.id
     if (id) {
-      this.props.getUser(id);
+      this.props.getUser(id)
     } else {
-      this.setState({ loading: false });
+      this.setState({ loading: false })
     }
   }
 
   componentDidMount() {
-    this._unsubscribeFocus = this.props.navigation.addListener('focus', () => {
-      this.setState({ loading: true });
-      this.load();
-    });
-    this._unsubscribeBlur = this.props.navigation.addListener('blur', () => {
-      this.props.navigation.setParams({ id: null });
-    });
+    this._unsubscribeFocus = this.props.navigation.addListener("focus", () => {
+      this.setState({ loading: true })
+      this.load()
+    })
   }
 
   componentDidUpdate(prevProps) {
-    const { loading } = this.state;
-    const { api } = this.props;
+    const { loading } = this.state
+    const { api } = this.props
 
     if (loading && prevProps.api.isLoading && !api.isLoading) {
-      this.setState({ loading: false });
+      this.setState({ loading: false })
     }
   }
 
   componentWillUnmount() {
     try {
-      this._unsubscribeFocus();
-      this._unsubscribeBlur();
+      this._unsubscribeFocus()
     } catch (err) {
       console.log(err)
     }
   }
 
   render() {
-    const { loading } = this.state;
-    const { isEdit, user } = this.props;
+    const { loading } = this.state
+    const { isEdit, user } = this.props
     return (
       <ScrollView style={styles.container} contentStyle={styles.content}>
         {loading ? (
@@ -64,24 +60,23 @@ export class UserDetail extends Component {
             <ActivityIndicator color={Color.steel} />
           </View>
         ) : (
+          <View>
             <View>
-              <View>
-                {!user.id && <Text>No user to display information.</Text>}
-              </View>
-              {user.id && (
-                <View>
-                  {isEdit ? (
-                    <EditUser {...this.props} />
-                  ) : (
-                      <ViewUser {...this.props} />
-                    )}
-                </View>
-              )}
+              {!user.id && <Text>No user to display information.</Text>}
             </View>
-          )}
-        <Button onPress={() => this.props.navigation.navigate('AppMenu')} title="Hello" />
+            {user.id && (
+              <View>
+                {isEdit ? (
+                  <EditUser {...this.props} />
+                ) : (
+                  <ViewUser {...this.props} />
+                )}
+              </View>
+            )}
+          </View>
+        )}
       </ScrollView>
-    );
+    )
   }
 }
 
@@ -90,30 +85,32 @@ export class UserDetail extends Component {
 // Update this code accordingly if you are not using neither modules.
 // Check README.md for more information
 const mapStateToProps = (state, ownProps) => {
+  console.log(state)
   const id = ownProps.route.params?.id
     ? ownProps.route.params?.id
-    : state.login?.user?.id;
+    : state.login?.user?.id
 
   return {
     token: state.login?.token,
     api: state.userProfile.api,
-    user: state.userProfile.users.find((user) => user.id === id) || {},
+    user: state.userProfile.users.find(user => user.id === id) || {},
     auth_user: state.login?.user,
-    isEdit: id && state.login?.user.id && id === state.login?.user.id,
-  };
-};
+    isEdit: id && state.login?.user.id && id === state.login?.user.id
+  }
+}
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    getUser: (id) => dispatch(user_read(id)),
-    updateUser: (data, token) => dispatch(user_update(data, token)),
-  };
-};
+    getUser: id => dispatch(user_read(id)),
+    updateUser: (data, token) => dispatch(user_update(data, token))
+  }
+}
 
 export default {
-  title: 'userProfile',
+  title: "userProfile",
   navigator: connect(mapStateToProps, mapDispatchToProps)(UserDetail),
   slice: {
-    reducer: reducer
-  },
-};
+    reducer: reducer,
+    actions: [user_read, user_update]
+  }
+}
