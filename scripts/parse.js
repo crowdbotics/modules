bd133842a28adf0615fd27612aa7857b4ea6d194
Daniel <daniel.s@crowdbotics.com>
2021-04-09 16:33:18 +0100

    Implement single shared modules json file

--- scripts/parse.js
+++ scripts/parse.js
@@ -2,43 +2,47 @@ import fs from "fs";
 import path from "path";
 import config from "./config.js";
 
-let data;
 const DJANGO_MODULES = path.join("modules", "django");
-const DJANGO_OUTPUT_FILE = path.join(config.dist.directory, "django.json");
 const REACT_NATIVE_MODULES = path.join("modules", "react-native");
-const REACT_NATIVE_OUTPUT_FILE = path.join(config.dist.directory, "react-native.json");
+const OUTPUT_FILE = path.join(config.dist.directory, "modules.json");
 const ACCEPTED_EXTENSIONS = [".json", ".js", ".ts", ".jsx", ".tsx", ".md", ".py"];
 
-const parseDir = dir => {
-  let entries = fs.readdirSync(dir);
-  entries.map(entry => {
-    let entryPath = path.join(dir, entry)
-    let stats = fs.statSync(entryPath);
-    if (stats.isDirectory()) {
-      parseDir(entryPath);
-    } else {
-      if (ACCEPTED_EXTENSIONS.includes(path.extname(entryPath))) {
-        data[entryPath] = fs.readFileSync(entryPath, "utf8");
-      }
-    }
-  });
-}
-
 const parseModules = dir => {
+  const parseModule = moduleDir => {
+    let entries = fs.readdirSync(moduleDir);
+    entries.map(entry => {
+      let entryPath = path.join(moduleDir, entry)
+      let stats = fs.statSync(entryPath);
+      if (stats.isDirectory()) {
+        parseModule(entryPath);
+      } else {
+        if (ACCEPTED_EXTENSIONS.includes(path.extname(entryPath))) {
+          data[entryPath] = fs.readFileSync(entryPath, "utf8");
+        }
+      }
+    });
+
+  }
+
+  let data = {};
   let modules = fs.readdirSync(dir);
+
   modules.map(module => {
-    parseDir(path.join(dir, module));
-  })
+    parseModule(path.join(dir, module))
+  });
+
+  return data;
 }
 
 const parseData = ({
-  data,
+  dir,
   file,
   parentDir,
   newFile = true,
   multiDirectory = false
 }) => {
-  let map = {};
+  let map = JSON.parse(fs.readFileSync(file, "utf8"));
+  let data = parseModules(dir);
   Object.keys(data).map(key => {
     let paths = key.split("/");
     let base = paths.splice(0, 2);
@@ -62,18 +66,14 @@ const parseData = ({
   fs.writeFileSync(file, JSON.stringify(map, null, 2));
 }
 
-data = {};
-parseModules(REACT_NATIVE_MODULES);
 parseData({
-  data: data,
-  file: REACT_NATIVE_OUTPUT_FILE,
+  dir: REACT_NATIVE_MODULES,
+  file: OUTPUT_FILE,
   parentDir: "/modules"
 });
-data = {};
-parseModules(DJANGO_MODULES);
 parseData({
-  data: data,
-  file: DJANGO_OUTPUT_FILE,
+  dir: DJANGO_MODULES,
+  file: OUTPUT_FILE,
   parentDir: "/backend/modules",
   multiDirectory: true
 });
