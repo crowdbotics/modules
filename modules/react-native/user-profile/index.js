7955fce647bee910752e9b032c79412e7f869b4e
Aline Tavares <alaisgomes@gmail.com>
2021-04-13 14:34:24 -0400

    More on user profile updates

--- modules/react-native/user-profile/index.js
+++ modules/react-native/user-profile/index.js
@@ -1,116 +1,58 @@
-import React, { Component } from "react"
-import { View, ScrollView, ActivityIndicator, Text, Button } from "react-native"
-import { user_read, user_update } from "./store/actions"
+import React, { useState, useEffect } from "react"
+import { View, ScrollView, ActivityIndicator, Text } from "react-native"
+import { getUserById, slice } from "./store"
+import { useSelector, useDispatch } from "react-redux"
 import { styles, Color } from "./styles"
-import { connect } from "react-redux"
-import reducer from "./store/reducers"
-import EditUser from "./edit"
+import { unwrapResult } from "@reduxjs/toolkit"
+import { EditUser } from "./edit"
 import ViewUser from "./view"
 
-export class UserDetail extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      loading: false
+export const UserProfile = ({ route }) => {
+  const [isEdit, setIsEdit] = useState(false)
+  // code below depends on the existence of any login module - update as needed.
+  const login = useSelector(state => {
+    return state?.login
+  })
+  const userId = route.params?.id || login?.user.id
+  const user = useSelector(state =>
+    state.userProfile.users.find(user => user.id === userId)
+  )
+  const api = useSelector(state => state.userProfile.api)
+  const dispatch = useDispatch()
+
+  useEffect(async () => {
+    if (userId) {
+      dispatch(getUserById(userId))
+        .then(unwrapResult)
+        .then(response => {
+          const edit = response.id === login?.user.id
+          setIsEdit(edit)
+        })
     }
-  }
+  }, [userId])
 
-  load() {
-    const { route } = this.props
-    const { auth_user } = this.props
-    const id = route.params?.id || auth_user.id
-    if (id) {
-      this.props.getUser(id)
-    } else {
-      this.setState({ loading: false })
-    }
-  }
-
-  componentDidMount() {
-    this._unsubscribeFocus = this.props.navigation.addListener("focus", () => {
-      this.setState({ loading: true })
-      this.load()
-    })
-  }
-
-  componentDidUpdate(prevProps) {
-    const { loading } = this.state
-    const { api } = this.props
-
-    if (loading && prevProps.api.isLoading && !api.isLoading) {
-      this.setState({ loading: false })
-    }
-  }
-
-  componentWillUnmount() {
-    try {
-      this._unsubscribeFocus()
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  render() {
-    const { loading } = this.state
-    const { isEdit, user } = this.props
-    return (
-      <ScrollView style={styles.container} contentStyle={styles.content}>
-        {loading ? (
-          <View>
-            <ActivityIndicator color={Color.steel} />
-          </View>
-        ) : (
-          <View>
+  return (
+    <ScrollView style={styles.container} contentStyle={styles.content}>
+      {api.loading === "pending" ? (
+        <View>
+          <ActivityIndicator color={Color.steel} />
+        </View>
+      ) : (
+        <View>
+          <View>{!user && <Text>No user to display information.</Text>}</View>
+          {user && (
             <View>
-              {!user.id && <Text>No user to display information.</Text>}
+              {isEdit ? <EditUser user={user} /> : <ViewUser user={user} />}
             </View>
-            {user.id && (
-              <View>
-                {isEdit ? (
-                  <EditUser {...this.props} />
-                ) : (
-                  <ViewUser {...this.props} />
-                )}
-              </View>
-            )}
-          </View>
-        )}
-      </ScrollView>
-    )
-  }
-}
-
-// Either gets user id passed through navigation or attempts to check login reducer:
-// thats the reducer for either 'Login and Signup' or 'Social Login' modules.
-// Update this code accordingly if you are not using neither modules.
-// Check README.md for more information
-const mapStateToProps = (state, ownProps) => {
-  console.log(state)
-  const id = ownProps.route.params?.id
-    ? ownProps.route.params?.id
-    : state.login?.user?.id
-
-  return {
-    token: state.login?.token,
-    api: state.userProfile.api,
-    user: state.userProfile.users.find(user => user.id === id) || {},
-    auth_user: state.login?.user,
-    isEdit: id && state.login?.user.id && id === state.login?.user.id
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    getUser: id => dispatch(user_read(id)),
-    updateUser: (data, token) => dispatch(user_update(data, token))
-  }
+          )}
+        </View>
+      )}
+    </ScrollView>
+  )
 }
 
 export default {
   title: "userProfile",
-  navigator: connect(mapStateToProps, mapDispatchToProps)(UserDetail),
-  slice: {
-    reducer: reducer,
-    actions: [user_read, user_update]
-  }
+  navigator: UserProfile,
+  slice
 }
