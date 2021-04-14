bf8709eb63b35b0736a753928e237aa60712d9c7
Aline Tavares <alaisgomes@gmail.com>
2021-04-14 12:26:29 -0400

    Update callback typo
    Co-authored-by: Daniel Sousa <daniel.s@crowdbotics.com>

--- modules/react-native/social-login/auth/utils.js
+++ modules/react-native/social-login/auth/utils.js
@@ -3,7 +3,7 @@ export const GOOGLE_WEB_CLIENT_ID = "XXXXXX.apps.googleusercontent.com"
 export const GOOGLE_IOS_CLIENT_ID = "YYYYYY.apps.googleusercontent.com"
 export const APPLE_SERVICE_ID = "com.crowdbotics.APP_NAME"
 export const APPLE_REDIRECT_CALLBACK =
-  "https://yur-app-here.com/accounts/apple/login/callback/"
+  "https://your-app-here.com/accounts/apple/login/callback/"
 
 // -----------------------------------------------------
 const messageMap = {
