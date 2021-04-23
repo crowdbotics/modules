81ea20cc05fbb21673842aed4e13c4e198e0283c
Aline Tavares <alaisgomes@gmail.com>
2021-04-23 13:59:12 -0400

    Add private to other packages and run bootstrap

--- demo/store/index.js
+++ demo/store/index.js
@@ -5,15 +5,4 @@ const getSlices = connectors =>
     .filter(([_, definition]) => definition.hasOwnProperty("slice"))
     .map(([_, definition]) => [definition.slice.name, definition.slice])
 
-const getThunks = connectors => {
-  let combinedThunks = {}
-  for (let [_, definition] of Object.entries(connectors)) {
-    Object.entries(definition).map(([key, value]) => {
-      if (key !== "slice") combinedThunks[key] = value
-    })
-  }
-  return combinedThunks
-}
-
 export const connectors = getSlices(mods)
-export const thunks = getThunks(mods)
