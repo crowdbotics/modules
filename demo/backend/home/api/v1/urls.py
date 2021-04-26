fc23506341a22a441099dfe2e5f560737f6c66d8
Daniel <daniel.s@crowdbotics.com>
2021-04-26 16:16:48 +0100

    Add cookiecutter generation on demo command

---
+++ demo/backend/home/api/v1/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from home.api.v1.viewsets import (
+    SignupViewSet,
+    LoginViewSet,
+    HomePageViewSet,
+    CustomTextViewSet,
+)
+
+router = DefaultRouter()
+router.register("signup", SignupViewSet, basename="signup")
+router.register("login", LoginViewSet, basename="login")
+router.register("customtext", CustomTextViewSet)
+router.register("homepage", HomePageViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
