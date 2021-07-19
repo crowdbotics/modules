b1707db19f6f3a0f7317622845dee487cb1751c7
Vlad Rimsha <vlad@jelly.ninja>
2021-07-19 19:06:14 +0700

    porting the project

--- modules/react-native-chat-v2/chat.js
+++ modules/react-native-chat-v2/chat.js
@@ -1,182 +1,100 @@
-import React, { useEffect, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  SafeAreaView,
-  TextInput,
-  KeyboardAvoidingView,
-  Platform
-} from "react-native";
-import { usePubNub } from "pubnub-react";
-// import Metadata from 'url-metadata'
+import React, { useState, useEffect } from 'react'
+import { GiftedChat, Actions } from 'react-native-gifted-chat'
+import metadata from './metadata'
+import PubNub from 'pubnub'
+import { usePubNub, PubNubProvider } from "pubnub-react"
+import isUrl from 'is-url'
+import { launchImageLibrary } from 'react-native-image-picker';
 
-const Chat = ({ route }) => {
-  // The `route` prop will be passed to us thanks to React Navigation.
-  // It will contain our emoji in `route.params.emoji`.
-  const userEmoji = route.params.emoji;
+const client = new PubNub({
+  subscribeKey: 'demo',
+  publishKey: 'demo',
+  uuid: 0
+})
 
-  // Here we obtain our PubNub instance thanks to using the provider
-  const pubnub = usePubNub();
+const user = {
+  _id: 0,
+  name: 'Vlad Rimsha',
+  avatar: 'https://ca.slack-edge.com/T2R0TP3DM-UDU3PDY81-25eda549c0b1-512',
+}
 
-  // In next two statements we define the state needed for our chat
-  const [input, setInput] = useState("");
+export default () => <PubNubProvider client={client}><_App /></PubNubProvider>
+
+const _App = () => {
+  const pubnub = usePubNub()
   const [messages, setMessages] = useState([]);
 
-  // First we need to set our PubNub UUID and subscribe to chat channel.
-  // We will use `useEffect` hook for that.
   useEffect(() => {
-    // We need to make sure that PubNub is defined
-    if (pubnub) {
-      // Set the UUID of our user to their chosen emoji
-      pubnub.setUUID(userEmoji);
+    if (!pubnub) return
 
-      // Create a listener that will push new messages to our `messages` variable
-      // using the `setMessages` function.
-      const listener = {
-        message: envelope => {
-          console.log('new envelope test 3', envelope)
-          const message = envelope.message
-          console.log('message', message)
-          setMessages(msgs => [
-            ...msgs,
-            {
-              id: envelope.message.id,
-              author: envelope.publisher,
-              content: envelope.message.content,
-              timetoken: envelope.timetoken
-            }
-          ]);
-        }
-      };
+    setMessages(previousMessages => GiftedChat.append(previousMessages, {
+      _id: 1,
+      text: 'Ben never tests anything',
+      createdAt: new Date(),
+      user
+    }))
 
-      // Add the listener to pubnub instance and subscribe to `chat` channel.
-      pubnub.addListener(listener);
-      pubnub.subscribe({ channels: ["chat"] });
-
-      // We need to return a function that will handle unsubscription on unmount
-      return () => {
-        pubnub.removeListener(listener);
-        pubnub.unsubscribeAll();
-      };
+    const listener = {
+      message: envelop => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, envelop.message))
+      },
+      file: envelop => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, {
+          ...envelop.userMetadata.user,
+          image: envelop.file.url
+        }))
+        console.log('got a new file', envelop)
+      }
     }
-  }, [pubnub]);
 
-  // This function handles sending messages.
-  const handleSubmit = () => {
-    // Clear the input field.
-    setInput("");
+    pubnub.setUUID(0)
+    pubnub.addListener(listener)
+    pubnub.subscribe({ channels: ['chat'] })
 
-    // Create the message with random `id`.
-    const message = {
-      content: input,
-      id: Math.random()
-        .toString(16)
-        .substr(2)
-    };
+    return () => {
+      pubnub.removeListener(listener)
+      pubnub.unsubscribeAll()
+    }
 
-    // Publish our message to the channel `chat`
-    pubnub.publish({ channel: "chat", message });
-  };
+  }, [pubnub])
+
+
+  const onSend = (messages = []) => {
+    messages.forEach(async message => {
+      if (isUrl(message.text)) {
+        const meta = await metadata(message.text)
+        if (meta.hybridGraph) {
+          message.image = meta.hybridGraph.image || meta.hybridGraph.imageSecureUrl
+        }
+      }
+      pubnub.publish({ channel: 'chat', message: { ...message, user } })
+    })
+  }
+
+  const pickImage = async () => {
+    await launchImageLibrary({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+    }, (res) => {
+      if (res.didCancel) return
+
+      pubnub.sendFile({
+        channel: 'chat',
+        file: { uri: result.uri, name: result.fileName, mimeType: result.type },
+        meta: {
+          user
+        }
+      })
+    })
+  }
 
   return (
-    <SafeAreaView style={styles.outerContainer}>
-      <KeyboardAvoidingView
-        style={styles.innerContainer}
-        behavior="height"
-        keyboardVerticalOffset={Platform.select({
-          ios: 78,
-          android: 0
-        })}
-      >
-        <View style={styles.topContainer}>
-          {messages.map(message => (
-            <View key={message.timetoken} style={styles.messageContainer}>
-              <View style={styles.avatar}>
-                <Text style={styles.avatarContent}>{message.author}</Text>
-              </View>
-              <View style={styles.messageContent}>
-                <Text>{message.content}</Text>
-              </View>
-            </View>
-          ))}
-        </View>
-        <View style={styles.bottomContainer}>
-          <TextInput
-            style={styles.textInput}
-            value={input}
-            onChangeText={setInput}
-            onSubmitEditing={handleSubmit}
-            returnKeyType="send"
-            enablesReturnKeyAutomatically={true}
-            placeholder="Type your message here..."
-          />
-          <View style={styles.submitButton}>
-            {input !== "" && <Button title="Send" onPress={handleSubmit} />}
-          </View>
-        </View>
-      </KeyboardAvoidingView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  outerContainer: {
-    width: "100%",
-    height: "100%"
-  },
-  innerContainer: {
-    width: "100%",
-    height: "100%"
-  },
-  topContainer: {
-    flex: 1,
-    width: "100%",
-    flexDirection: "column",
-    justifyContent: "flex-end",
-    paddingHorizontal: 16
-  },
-  messageContainer: {
-    flexDirection: "row",
-    marginTop: 16,
-    alignItems: "center",
-    backgroundColor: "#fff",
-    padding: 8,
-    borderRadius: 4
-  },
-  avatar: {
-    width: 38,
-    height: 38,
-    borderRadius: 50,
-    overflow: "hidden",
-    marginRight: 16
-  },
-  avatarContent: {
-    fontSize: 30,
-    textAlign: "center",
-    textAlignVertical: "center"
-  },
-  messageContent: {
-    flex: 1
-  },
-  bottomContainer: {
-    width: "100%",
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 16
-  },
-  textInput: {
-    flex: 1,
-    backgroundColor: "#fff",
-    borderRadius: 4,
-    padding: 16,
-    elevation: 2
-  },
-  submitButton: {
-    position: "absolute",
-    right: 32
-  }
-});
-
-export default Chat;
+    <GiftedChat
+      messages={messages}
+      renderUsernameOnMessage={true}
+      onSend={onSend}
+      renderActions={() => <Actions onPressActionButton={pickImage} />}
+      user={user}
+    />
+  )
+}
