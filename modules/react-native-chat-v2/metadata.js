bd48f7b7b9b7e66ed4fb330682e054d02e2d1104
Vlad Rimsha <vlad@jelly.ninja>
2021-07-22 20:52:21 +0700

    add proper readme

--- modules/react-native-chat-v2/metadata.js
+++ modules/react-native-chat-v2/metadata.js
@@ -1,3 +1,13 @@
-import urlMetadata from 'url-metadata';
+const app_id = 'da4b3019-cfd7-430e-8153-d80dcef356e7'
 
-export default fetchURL = (url) => urlMetadata(url)
+export default async (url) => {
+	try {
+		const res = await fetch(`https://opengraph.io/api/1.1/site/${encodeURIComponent(url)}?app_id=${app_id}`)
+		const data = await res.json()
+
+		return data
+	} catch (e) {
+		console.log('failed to get any metadata from link', e)
+		return {}
+	}
+}
