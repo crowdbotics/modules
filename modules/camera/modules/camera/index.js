1e1bfbe996a174eef23fc39d57949f313bdb9714
shahraizali <shahraizali10@yahoo.com>
2021-07-29 00:27:29 +0500

    moved url to global options

--- modules/camera/modules/camera/index.js
+++ modules/camera/modules/camera/index.js
@@ -1,13 +1,14 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 import ActionSheet from 'react-native-actionsheet'
 import { pickFromCamera, pickFromGallery, uploadImage} from './cameraUtils'
-
+import { GlobalOptionsContext } from "@options";
 
 const shCamera = ({ navigation }) => {
   // More info on all the options is below in the API Reference... just some common use cases shown here
   const actionSheet = useRef(null);
+  const gOptions = useContext(GlobalOptionsContext);
 
   const ImagePickerOptions = ['Take Photo', 'Choose from Gallery', 'Cancel'];
 
@@ -39,12 +40,12 @@ const shCamera = ({ navigation }) => {
               let res = await pickFromCamera();
               console.log('pickFromCamera res', res);
               // TODO: Upload Image
-              res && uploadImage(res)
+              res && uploadImage(res, gOptions)
             } else if (index == 1) {
               let res = await pickFromGallery();
               console.log('_pickFromGallery res', res);
               // TODO: Upload Image
-              res && uploadImage(res)
+              res && uploadImage(res, gOptions)
             }
           }
         }}
