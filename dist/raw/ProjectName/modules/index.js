ebf02a22fc7314404f4892160f2605395980a72c
shahraizali <shahraizali10@yahoo.com>
2021-09-04 02:07:11 +0500

    reverted dist changes

--- dist/raw/ProjectName/modules/index.js
+++ dist/raw/ProjectName/modules/index.js
@@ -1,11 +1,6 @@
-import {
-  getPropertyMap,
-  getModules
-} from "./utils.js";
+import { getPropertyMap, getModules } from "./utils.js";
 import * as mods from "glob:./**/index.js";
-import {
-  screens
-} from "@screens";
+import { screens } from "@screens";
 
 export const modules = getModules(mods);
 export const initialRoute = modules[0].title;
@@ -14,9 +9,9 @@ export const navigators = Object.entries(getPropertyMap(modules, "navigator"));
 export const hooks = Object.entries(getPropertyMap(modules, "hook"));
 
 export function getNavigationScreen(name) {
-  const screen = screens.find(x => x[0].includes(name))
+    const screen = screens.find(x => x[0].includes(name))
 
-  if (screen) {
+  if(screen){
     return screen[0]
   }
 
