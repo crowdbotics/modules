5076624bd783b2d2e560f87347a8dd470cf8ff98
Daniel <daniel.s@crowdbotics.com>
2021-09-30 03:20:30 +0100

    Update validateEmail option to be constructor compatible

--- modules/react-native-login/options.js
+++ modules/react-native-login/options.js
@@ -29,7 +29,7 @@ export default {
   LOGO_URL: "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/cb-icon.png",
   BACKGROUND_URL: "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/halfbg.png",
   HOME_SCREEN_NAME: "Home Screen",
-  validateEmail: /^[^\s]+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+  validateEmail: "^[^\\s]+([\.-]?\\w+)*@\\w+([\.-]?\\w+)*(\.\\w{2,3})+$",
   SignInNavText: "Sign In",
   SignUpNavText: "Sign Up",
   SignInButtonText: "Login",
