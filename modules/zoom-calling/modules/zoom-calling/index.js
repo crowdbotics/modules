152706bd7abc041f8e33a3fd59004699fdb705ab
Saad Bin Abid <saad.abid@crowdbotics.com>
2022-02-17 20:56:00 +0500

    set access token

--- modules/zoom-calling/modules/zoom-calling/index.js
+++ modules/zoom-calling/modules/zoom-calling/index.js
@@ -1,9 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Button } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button, Image, TouchableOpacity, Modal, Pressable } from "react-native";
 import ZoomUs from 'react-native-zoom-us';
+// @ts-ignore
 import { WebView } from 'react-native-webview';
+import { getCurrentUser, getOauthToken } from './utils';
+import { StyleSheet } from 'react-native';
+// @ts-ignore
+import DialogInput from 'react-native-dialog-input';
 
 const ZoomCalling = () => {
+  const [isFirst, setIsFirst] = useState(true)
+  const [oauthToken, setOauthToken] = useState(false)
+  const [isJoinMeeting, setIsJoinMeeting] = useState(false)
+  const [modalVisible, setModalVisible] = useState(false);
+  const [currentUser, setCurrentUser] = useState({
+    id: '',
+    email: null,
+    pmi: null,
+    host_key: null,
+    pic_url: null,
+    first_name: "",
+    last_name: "",
+    personal_meeting_url: null
+  })
 
   useEffect(() => {
     ZoomUs.initialize({
@@ -18,41 +37,263 @@ const ZoomCalling = () => {
     // Start Meeting
     console.log('startMeeting here...')
     ZoomUs.startMeeting({
-      userName: 'Johny',
-      meetingNumber: '7776788669',
-      userId: '480386',
-      zoomAccessToken: '5pUP5hFRRC2nC2H4FR2L1w',
+      userName: currentUser.first_name + '' + currentUser.last_name,
+      meetingNumber: currentUser.pmi,
+      userId: currentUser.id,
+      zoomAccessToken: oauthToken['access_token'], // '5pUP5hFRRC2nC2H4FR2L1w',
     })
   }
 
-  const joinMeeting = () => {
+  const joinMeeting = (meetingId) => {
     // Start Meeting
     console.log('joinMeeting here...')
+    setIsJoinMeeting(false)
     ZoomUs.joinMeeting({
-      userName: 'Johny',
-      meetingNumber: '7776788669',
+      userName: currentUser.first_name + '' + currentUser.last_name,
+      meetingNumber: meetingId
     })
   }
 
+  const parse_query_string = (url) => {
+    let regex = /[?&]([^=#]+)=([^&#]*)/g,
+      params = {},
+      match;
+    while (match = regex.exec(url)) {
+      params[match[1]] = match[2];
+    }
+    return params;
+  }
+
+  const onNavigationStateChange = (evt) => {
+    if (evt.url.includes('https://oauth.pstmn.io/v1/callback')) {
+      let params = parse_query_string(evt.url);
+      if (params.code && isFirst) {
+        setIsFirst(false)
+        getOauthToken(params.code).then((response) => {
+          setOauthToken(response)
+        }).catch(err => console.log('err', err))
+        return
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (oauthToken) {
+      getCurrentUser(oauthToken['access_token']).then(response => {
+        console.log('getCurrentUser', response)
+        setCurrentUser(response)
+      })
+    }
+  }, [oauthToken])
+
   return (
-    <View style={{flex: 1}}>
-      {/* <Text onPress={startMeeting}>Start Meeting</Text>
-      <Text onPress={joinMeeting}>Join Meeting</Text> */}
-      <WebView
-        userAgent="Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19" 
-        onMessage={(event)=>{
-          let message  = event.nativeEvent.data;
-          console.log('event', event)
-        }}
-        onNavigationStateChange={(evt)=>{}}
-        onError={(e) => {
-            console.warn('error occured', e)
-        }}
-        source={{uri: `https://zoom.us/oauth/authorize?response_type=code&client_id=O5o5klrbQWq3L6PBWbRjoA&redirect_uri=https://oauth.pstmn.io/v1/callback` }}
+    <View style={{ flex: 1 }}>
+      {oauthToken ? <>
+        <View style={styles.header}>
+          <View>
+            <Text>&nbsp;</Text>
+          </View>
+          <View style={styles.UserImageArea}>
+            <View>
+              <Text>{currentUser.first_name + '' + currentUser.last_name}</Text>
+            </View>
+            <Image
+              style={styles.userLogo}
+              resizeMode="cover"
+              borderRadius={10}
+              source={{
+                uri: currentUser.pic_url,
+              }}
+            />
+          </View>
+        </View>
+        <View style={styles.MainCard}>
+          <View style={styles.Card}>
+            <TouchableOpacity onPress={startMeeting}>
+              <View style={styles.cardbody}>
+                <View style={styles.innercard}>
+                  <View>
+                    <Text style={styles.HostMeetingText}>Host a meeting</Text>
+                  </View>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.Card}>
+            <TouchableOpacity onPress={() => setIsJoinMeeting(true)}>
+              <View style={styles.cardbody}>
+                <View style={styles.innercard}>
+                  <View>
+                    <Text style={styles.JoinMeetingText}>Join a meeting</Text>
+                  </View>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.MeetingCard}>
+          <TouchableOpacity onPress={() => setModalVisible(true)}>
+            <View style={styles.cardbody}>
+              <View style={styles.innercard}>
+                <View>
+                  <Text style={styles.ScheduleMeetingText}>Schedule a meeting</Text>
+                </View>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <DialogInput isDialogVisible={isJoinMeeting}
+            title={"Join Meeting"}
+            message={"Please enter Meeting ID"}
+            hintInput ={"Meeting ID"}
+            submitInput={ (meetingId) => joinMeeting(meetingId) }
+            closeDialog={ () => setIsJoinMeeting(false)}>
+        </DialogInput>
+        <View style={styles.centeredView}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}></Text>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text style={styles.textStyle}>Hide Modal</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+        </View>
+        
+      </> : <WebView
+        useWebKit = {true}
+        userAgent="Mozilla/5.0 (Linux; Android) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/98.0.4758.87 Mobile Safari/537.36"
+        onNavigationStateChange={onNavigationStateChange}
+        source={{ uri: `https://zoom.us/oauth/authorize?response_type=code&client_id=O5o5klrbQWq3L6PBWbRjoA&redirect_uri=https://oauth.pstmn.io/v1/callback` }}
       />
+      }
+
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 5
+  },
+  userLogo: {
+    width: 40,
+    height: 40,
+    margin: 5
+  },
+  cardbody: {
+    minHeight: 50,
+    height: '100%',
+    backgroundColor: '#2D8CFF',
+    borderRadius: 5
+  },
+  innercard: {
+    padding: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 1.62,
+    elevation: 15,
+    display: 'flex',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  UserImageArea:{
+    display: 'flex', 
+    flexDirection: 'row', 
+    alignItems: 'center' 
+  },
+  MainCard:{
+    display: "flex", 
+    flexDirection: "row", 
+    justifyContent: "space-evenly", 
+    marginTop: 30
+  },
+  Card:{
+    width: "50%", 
+    height: 100, 
+    padding: 5
+  },
+  HostMeetingText:{
+    color: 'white', 
+    textTransform: 'uppercase'
+  },
+  JoinMeetingText:{
+    color: 'white', 
+    textTransform: 'uppercase'
+  },
+  ScheduleMeetingText:{
+    color: 'white', 
+    textTransform: 'uppercase'
+  },
+  MeetingCard:{
+    width: "100%",
+    height: 50, 
+    padding: 5,
+  },
+  ///
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
+});
 
 export default {
   title: "ZoomCalling",
---
+++ modules/zoom-calling/modules/zoom-calling/utils.js
@@ -0,0 +1,26 @@
+import base64 from 'react-native-base64'
+
+export const getOauthToken = async (code) => {
+  let res = await fetch(`https://api.zoom.us/oauth/token?code=${code}&grant_type=authorization_code&redirect_uri=https://oauth.pstmn.io/v1/callback`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json',
+      'Authorization': `Basic ${base64.encode('O5o5klrbQWq3L6PBWbRjoA:ia00qOHowj0vEf1BXOXuZUxc54RFMZHC')}`
+    },
+    body: JSON.stringify({})
+  })
+  return res.json()
+}
+
+export const getCurrentUser = async (token) => {
+  let res = await fetch(`https://api.zoom.us/v2/users/me`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${token}`
+    }
+  })
+  return res.json()
+}
