8392defb0403520bd01eb3aeac828cbbed58a010
Vlad Rimsha <vlad@jelly.ninja>
2022-02-23 13:48:17 +0700

    Fix bug of not unsubscribing on state change

--- modules/react-native-chat-v2/App.jsx
+++ modules/react-native-chat-v2/App.jsx
@@ -30,6 +30,7 @@ export default () => {
             channelGroups: [uuid],
             withPresence: true
         });
+        return () => client.unsubscribeAll()
     }, []);
     return <>
     <StatusBar translucent={true}/>
