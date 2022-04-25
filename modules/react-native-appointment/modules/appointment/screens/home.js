ed33f90e03c11f332619255e605c9346b9ff5995
Saad Bin Abid <saad.abid@crowdbotics.com>
2022-04-25 10:40:57 +0500

    Add google signin

--- modules/react-native-appointment/modules/appointment/screens/home.js
+++ modules/react-native-appointment/modules/appointment/screens/home.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, useWindowDimensions } from 'react-native';
 // @ts-ignore
 import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
 import AppointmentList from './appointmentList';
 import Calendar from './calendar';
+// @ts-ignore
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
+
 
 const Home = (props) => {
   const layout = useWindowDimensions();
@@ -13,9 +16,34 @@ const Home = (props) => {
     { key: 'Calendar', title: 'Calendar' },
 
   ]);
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/calendar'],
+      androidClientId: '980184327649-ssjrkhp0s3utqe5e3k3312ggafvguciv.apps.googleusercontent.com',
+      offlineAccess: true,
+      webClientId: '980184327649-hrvksdtlg79vn7v36s305nf99p2eu283.apps.googleusercontent.com'
+    });
+
+    GoogleSignin.hasPlayServices().then((hasPlayService) => {
+      if (hasPlayService) {
+        GoogleSignin.signIn().then(async (userInfo) => {
+          // console.log(JSON.stringify(userInfo))
+          const tokens = await GoogleSignin.getTokens()
+          console.log('tokens', tokens)
+        }).catch((e) => {
+          console.log("ERROR IS: " + JSON.stringify(e));
+        })
+      }
+    }).catch((e) => {
+      console.log("ERROR IS: " + JSON.stringify(e));
+    })
+
+  }, [])
+
   const renderScene = SceneMap({
-    AppointmentList: ()=><AppointmentList navigation={props.navigation}/>,
-    Calendar: () => <Calendar navigation={props.navigation}/>,
+    AppointmentList: () => <AppointmentList navigation={props.navigation} />,
+    Calendar: () => <Calendar navigation={props.navigation} />,
 
   });
 
@@ -30,7 +58,7 @@ const Home = (props) => {
   );
 
   return (
-    
+
     <TabView
       navigationState={{ index, routes }}
       renderScene={renderScene}
@@ -38,7 +66,7 @@ const Home = (props) => {
       onIndexChange={setIndex}
       initialLayout={{ width: layout.width }}
     />
-    
+
   )
 
 };
@@ -49,12 +77,12 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginLeft: 10,
     marginRight: 10,
-    marginVertical:10
+    marginVertical: 10
   },
   indicator: { backgroundColor: '#FFFFFF', borderRadius: 10, height: 37, shadowColor: '#000', elevation: 5 },
   indicatorContainer: { height: 37, marginTop: 6 },
   label: { color: '#000000', fontSize: 14, textTransform: 'capitalize', width: '100%' },
-  
+
 
 })
 export default Home
