b219693962b0df56f0f00f0df66756cd1d89ee1b
Saad Bin Abid <saad.abid@crowdbotics.com>
2022-04-29 10:41:14 +0500

    restructure screen compoment

--- modules/screen-settings/README.md
+++
@@ -1,3 +0,0 @@
-# Settings
-
-## Settings screen
--- modules/screen-settings/index.js
+++ modules/screen-settings/index.js
@@ -1,15 +1,14 @@
-import * as React from 'react';
-import { Text, View, Image, StyleSheet } from 'react-native';
+import * as React from "react";
+import { Text, View, Image, StyleSheet } from "react-native";
 
-
- const Settings = () => {
+export const Settings = () => {
   return (
     <View style={styles.container}>
 
       <View style={styles.mainSection}>
         <View style={styles.faqSupport}>
           <Image style={styles.faqIcon} source={require("./assets/faq.png")}/>
-          <Text style={styles.faqText}>FAQ's</Text>
+          <Text style={styles.faqText}>FAQ&apos;s</Text>
         </View>
         <View style={styles.emailSupport}>
           <Image style={styles.emailsupportIcon} source={require("./assets/emailsupport.png")}/>
@@ -44,129 +43,125 @@ import { Text, View, Image, StyleSheet } from 'react-native';
       </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   topHead: {
-    display:'flex',
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'center',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
   },
   mainHeading: {
-    fontSize: 24,
+    fontSize: 24
   },
   container: {
-    padding:10,
-    backgroundColor:'#FFF',
-    height:'100%'
+    padding: 10,
+    backgroundColor: "#FFF",
+    height: "100%"
   },
-  mainSection:{
-    marginTop:20,
+  mainSection: {
+    marginTop: 20
   },
   emailSupport: {
-    display:'flex',
-    flexDirection:'row',
-    alignItems:'center',
-    borderBottomWidth:1,
-    marginLeft:10,
-    marginRight:10,
-    borderBottomColor: 'rgba(51,51,51,0.05)',
-    marginTop:20,  
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    borderBottomColor: "rgba(51,51,51,0.05)",
+    marginTop: 20
   },
   emailsupportIcon: {
-    height:20,
-    width:20,
-    marginRight:20,
-    marginBottom:20,
+    height: 20,
+    width: 20,
+    marginRight: 20,
+    marginBottom: 20
   },
   emailsupportText: {
-    fontSize:18,
-    fontWeight:'500',
-    marginBottom:20,
+    fontSize: 18,
+    fontWeight: "500",
+    marginBottom: 20
   },
   faqSupport: {
-    display:'flex',
-    flexDirection:'row',
-    alignItems:'center',
-    borderBottomWidth:1,
-    marginLeft:10,
-    marginRight:10,
-    borderBottomColor: 'rgba(51,51,51,0.05)',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    borderBottomColor: "rgba(51,51,51,0.05)"
   },
   faqIcon: {
-    height:20,
-    width:20,
-    marginRight:20,
-    marginBottom:20,
+    height: 20,
+    width: 20,
+    marginRight: 20,
+    marginBottom: 20
   },
   faqText: {
-    fontSize:18,
-    fontWeight:'500',
-    marginBottom:20,
+    fontSize: 18,
+    fontWeight: "500",
+    marginBottom: 20
   },
   invitefriendsSupport: {
-    display:'flex',
-    flexDirection:'row',
-    alignItems:'center',
-    borderBottomWidth:1,
-    marginLeft:10,
-    marginRight:10,
-    marginTop:20,
-    borderBottomColor: 'rgba(51,51,51,0.05)',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 20,
+    borderBottomColor: "rgba(51,51,51,0.05)"
   },
   inviteFriendsIcon: {
-    height:20,
-    width:20,
-    marginRight:20,
-    marginBottom:20,
+    height: 20,
+    width: 20,
+    marginRight: 20,
+    marginBottom: 20
   },
   inviteFriendsText: {
-    fontSize:18,
-    fontWeight:'500',
-    marginBottom:20,
+    fontSize: 18,
+    fontWeight: "500",
+    marginBottom: 20
   },
   pushNotifications: {
-    display:'flex',
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-between',
-    borderBottomWidth:1,
-    marginLeft:10,
-    marginRight:10,
-    marginTop:20,
-    borderBottomColor: 'rgba(51,51,51,0.05)',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderBottomWidth: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 20,
+    borderBottomColor: "rgba(51,51,51,0.05)"
   },
-  pushnoti:{
-    display:'flex',
-    flexDirection:'row',
+  pushnoti: {
+    display: "flex",
+    flexDirection: "row"
   },
   pushnotificationsIcon: {
-    height:20,
-    width:20,
-    marginBottom:20,
-    marginRight:20,
+    height: 20,
+    width: 20,
+    marginBottom: 20,
+    marginRight: 20
   },
   pushnotificationsText: {
-    fontSize:18,
-    fontWeight:'500',
-    marginBottom:20,
+    fontSize: 18,
+    fontWeight: "500",
+    marginBottom: 20
   },
-  switchbutton:{
-    height:22,
-    width:44,
-    backgroundColor:'#E6E6E6',
-    borderRadius:12,
-     marginBottom:20,
+  switchbutton: {
+    height: 22,
+    width: 44,
+    backgroundColor: "#E6E6E6",
+    borderRadius: 12,
+    marginBottom: 20
   },
   ballicon: {
-    height:20,
-    width:20,
-    backgroundColor:'#000000',
-    borderRadius:10,
-  },
+    height: 20,
+    width: 20,
+    backgroundColor: "#000000",
+    borderRadius: 10
+  }
 });
-export default {
-  title: "Settings",
-  navigator: Settings
-}
