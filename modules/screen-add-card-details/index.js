362f0f09fdb7b03c94c7b18612d2726038a08b57
Shahraiz Ali <shahraiz.ali@cheetay.pk>
2022-04-29 02:23:36 +0200

    module

---
+++ modules/screen-add-card-details/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+export const AddCardDetailsScreen = (params) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headingTxt}>Add Card Details Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+    flex: 1,
+    justifyContent: "space-around"
+  },
+  headingTxt: {
+    fontSize: 24,
+    fontWeight: "bold",
+    padding: 2,
+    marginVertical: 12
+  }
+});
