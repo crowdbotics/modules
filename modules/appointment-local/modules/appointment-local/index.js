a8eb41f2f955b1b0f6625ed46e0243fe368b9d7f
Saad Bin Abid <saad.abid@crowdbotics.com>
2022-04-29 17:00:25 +0500

    handle error log

--- modules/appointment-local/modules/appointment-local/index.js
+++ modules/appointment-local/modules/appointment-local/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import Navigator from './Navigator';
-
-
+import { LogBox } from 'react-native';
 
 const Appointment = () => {
+  LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
 
-    return(
-      <Navigator />
-    )
-  
+  return (
+    <Navigator />
+  )
+
 };
-  
-
 
 export default {
   title: "Appointment",
