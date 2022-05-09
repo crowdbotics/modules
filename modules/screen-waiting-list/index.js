85248c102b5ef61427ce8b76d2b52dc02c40393b
Saad Bin Abid <saad.abid@crowdbotics.com>
2022-05-09 17:55:19 +0500

    lint fix component

--- modules/screen-waiting-list/index.js
+++ modules/screen-waiting-list/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { Text, StyleSheet, View, TouchableHighlight, Image, ScrollView } from "react-native";
 
-const WaitingList= (params) => {
+const WaitingList = (params) => {
   return (
     <ScrollView>
       <View style={styles.container}>
         <View style={styles.header}>
           <View style={styles.image}>
-            <Image source={require('./assets/edit.png')} />
+            <Image source={require("./assets/edit.png")} />
           </View>
           <View style={styles.description}>
             <Text>Business name</Text>
             <View style={styles.orderInfo}>
-              <Image source={require('./assets/time.png')} style={styles.img}/>
+              <Image source={require("./assets/time.png")} style={styles.img}/>
               <Text>45-55 min</Text>
             </View>
             <View style={styles.orderInfo}>
-              <Image source={require('./assets/location.png')} style={styles.img}/>
+              <Image source={require("./assets/location.png")} style={styles.img}/>
               <Text>12126 FIrst St, LA</Text>
             </View>
           </View>
@@ -57,37 +57,37 @@ const WaitingList= (params) => {
 const styles = StyleSheet.create({
   container: {
     padding: 10,
-    backgroundColor:'#fff',
-    flex: 1,
+    backgroundColor: "#fff",
+    flex: 1
   },
-  header:{
-    flexDirection:'row',
-    alignItems:'center',
-    padding:12,
-    shadowColor:"rgba(0, 0, 0, 0.2)",
-    elevation:5,
-    borderRadius:8,
-    height:100,
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 12,
+    shadowColor: "rgba(0, 0, 0, 0.2)",
+    elevation: 5,
+    borderRadius: 8,
+    height: 100
   },
-  image:{
-    flexDirection:'column',
-    justifyContent:'center',
-    alignItems:'center',
-    marginRight:10,
-    backgroundColor:'#F9D8D9',
-    borderRadius:10,
-    width:80,
-    height:'100%',
-    marginLeft:10
-    
+  image: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 10,
+    backgroundColor: "#F9D8D9",
+    borderRadius: 10,
+    width: 80,
+    height: "100%",
+    marginLeft: 10
+
   },
-  description:{
-    borderRadius:10,
-    height:'100%'
+  description: {
+    borderRadius: 10,
+    height: "100%"
   },
   text: {
-    marginLeft:10,
-    marginVertical:20
+    marginLeft: 10,
+    marginVertical: 20
   },
   tabView: {
     width: "70%",
@@ -98,7 +98,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderRadius: 10,
     paddingVertical: 6,
-    marginLeft:10,
+    marginLeft: 10
   },
   follow: {
     borderRadius: 10,
@@ -115,26 +115,26 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#F1F1F1"
   },
-  btn:{
-    paddingHorizontal:'10%'
+  btn: {
+    paddingHorizontal: "10%"
   },
-  orderInfo:{
-    flexDirection:'row',
-    
+  orderInfo: {
+    flexDirection: "row"
+
   },
   img: {
-    marginRight:8
+    marginRight: 8
   }
 
 });
 export default WaitingList;
 
 const OrderCard = () => {
-  return(
+  return (
     <View style={orderStyles.container}>
       <View style={orderStyles.order}>
         <View style={orderStyles.image}>
-          <Image source={require('./assets/edit.png')}/>
+          <Image source={require("./assets/edit.png")}/>
         </View>
         <View style={orderStyles.description}>
           <Text>Order name</Text>
@@ -144,7 +144,7 @@ const OrderCard = () => {
               <Text>3</Text>
               <Text>+</Text>
             </View>
-            <Image source={require('./assets/delete.png')} style={orderStyles.img}/>
+            <Image source={require("./assets/delete.png")} style={orderStyles.img}/>
           </View>
 
         </View>
@@ -153,65 +153,65 @@ const OrderCard = () => {
         <Text style={orderStyles.orderPrice}>$10.25</Text>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const orderStyles = StyleSheet.create({
-  container:{
-    flexDirection:'row',
-    justifyContent:'space-between',
-    alignItems:'center',
-    padding:12,
-    marginBottom:20,
-    shadowColor:"rgba(0, 0, 0, 0.2)",
-    elevation:5,
-    borderRadius:10
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 12,
+    marginBottom: 20,
+    shadowColor: "rgba(0, 0, 0, 0.2)",
+    elevation: 5,
+    borderRadius: 10
   },
-  order:{
-    flexDirection:'row',
-    alignItems:'center',
-    padding:5
+  order: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 5
   },
-  image : {
-    height:60,
-    width:60,
-    borderRadius:30,
-    backgroundColor: 'rgba(0, 0, 0, 0.2)',
-    justifyContent:'center',
-    alignItems:'center',
-    marginRight:10
+  image: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 10
   },
   description: {
-    flexDirection:'column',
-    justifyContent:'space-between',
-  
+    flexDirection: "column",
+    justifyContent: "space-between"
+
   },
-  quantity:{
-    width:80,
-    height:30,
-    backgroundColor: '#F1F1F1',
-    borderRadius:10,
-    flexDirection:'row',
-    justifyContent:'space-between',
-    alignItems:'center',
-    paddingHorizontal:10,
-    marginTop:12
-    
+  quantity: {
+    width: 80,
+    height: 30,
+    backgroundColor: "#F1F1F1",
+    borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    marginTop: 12
+
   },
-  bottomComponent:{
-    flexDirection:'row',
-    alignItems:'center'
+  bottomComponent: {
+    flexDirection: "row",
+    alignItems: "center"
   },
-  img:{
-    marginLeft:10,
-    marginTop:10
+  img: {
+    marginLeft: 10,
+    marginTop: 10
   },
-  orderPrice:{
-    fontSize:30,
-    fontWeight:'bold',
-    marginRight:10
+  orderPrice: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginRight: 10
   }
-})
+});
 
 const Button = (props) => {
   return (
@@ -237,7 +237,6 @@ const btnStyles = StyleSheet.create({
   },
   text: {
     fontWeight: "bold",
-    fontSize: 15,
+    fontSize: 15
   }
 });
-
