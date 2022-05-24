9af09ad089f257adb1a5d06345067996d6bfc4ed
Saad Bin Abid <saad.abid@crowdbotics.com>
2022-05-24 17:53:08 +0500

    wallet switch screen lint fix

--- modules/screen-wallet-switch/index.js
+++ modules/screen-wallet-switch/index.js
@@ -20,7 +20,7 @@ const WalletSwitchScreen = () => {
           <View style={styles.tabItem}>
             <Text>Send</Text>
           </View>
-          <View  style={styles.tabItem}>
+          <View style={styles.tabItem}>
             <Text>Receive</Text>
           </View>
           <View style={[styles.tabItem, styles.selectedTab]}>
@@ -33,7 +33,7 @@ const WalletSwitchScreen = () => {
         <Text style={styles.ReceiveTransaction}>Switch</Text>
         <View style={styles.wp50}>
           <View style={styles.tabView}>
-            <View style={[styles.tabItem,styles.selectedTab]}>
+            <View style={[styles.tabItem, styles.selectedTab]}>
               <Text>Buy</Text>
             </View>
             <View style={styles.tabItem}>
@@ -45,7 +45,7 @@ const WalletSwitchScreen = () => {
       <View style={styles.bottomContainer}>
         <View>
           <Text style={styles.heading}>Buy ETH</Text>
-          <View style={styles.conversion}> 
+          <View style={styles.conversion}>
             <Text style={styles.conversionText}>ETH 2 = ETH 1.32</Text>
             <Text style={styles.conversionText}>+%11.89</Text>
           </View>
@@ -58,7 +58,7 @@ const WalletSwitchScreen = () => {
           <Text style={styles.amount}>16,0|</Text>
         </View>
         <View style={styles.img}>
-          <Image source={require('./assets/Icon.png')}/>
+          <Image source={require("./assets/Icon.png")}/>
         </View>
         <View style={styles.buyCard}>
           <View>
@@ -114,8 +114,8 @@ const styles = StyleSheet.create({
   balanceTxt: {
     color: "#7C7C7C", fontSize: 12, alignSelf: "flex-end", textAlign: "right", fontWeight: "bold"
   },
-  wp50:{
-    width:'50%'
+  wp50: {
+    width: "50%"
   },
   walletBalance: {
     display: "flex", flexDirection: "row", justifyContent: "space-between", marginBottom: 28, marginTop: 14, paddingHorizontal: 23
@@ -132,7 +132,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderRadius: 10,
     padding: 6
-    
+
   },
   selectedTab: {
     backgroundColor: "#FFF",
@@ -147,7 +147,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#F1F1F1",
     borderRadius: 10,
     fontWeight: "bold",
-    flex:1
+    flex: 1
   },
   receive: { display: "flex", flex: 1, alignSelf: "center", marginTop: 25 },
   ReceiveTransaction: {
@@ -159,9 +159,9 @@ const styles = StyleSheet.create({
     borderColor: "#C4C4C4", borderWidth: 2, padding: 25, borderRadius: 10
   },
   btnContainer: {
-    marginTop:'10%',
-    paddingHorizontal:'10%',
-    marginBottom:20
+    marginTop: "10%",
+    paddingHorizontal: "10%",
+    marginBottom: 20
   },
   cardWallet: {
     padding: 14,
@@ -170,9 +170,9 @@ const styles = StyleSheet.create({
     height: 134,
     borderRadius: 10
   },
-  image:{
-    height:30,
-    width:30
+  image: {
+    height: 30,
+    width: 30
   },
   walletDetails: {
     paddingTop: 12,
@@ -196,39 +196,39 @@ const styles = StyleSheet.create({
   contentContainer: {
     display: "flex", flexDirection: "column"
   },
-  bottomContainer:{
-    padding:10
+  bottomContainer: {
+    padding: 10
   },
-  heading:{
-    fontSize:24,
-    fontWeight:'bold',
-    margin:10
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold",
+    margin: 10
   },
-  conversion:{
-    flexDirection:'row',
-    alignItems:"center"
+  conversion: {
+    flexDirection: "row",
+    alignItems: "center"
   },
-  conversionText:{
-    color:'#999999',
-    marginHorizontal:10
+  conversionText: {
+    color: "#999999",
+    marginHorizontal: 10
   },
-  buyCard:{
-    backgroundColor:'#fff',
-    borderRadius:10,
-    padding:20, 
-    flexDirection:"row",
-    justifyContent:'space-between',
-    alignItems:'center',
-    marginVertical:10
+  buyCard: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 10
   },
-  amount:{
-    fontSize:22,
-    fontWeight:"bold"
+  amount: {
+    fontSize: 22,
+    fontWeight: "bold"
   },
-  img:{
-    justifyContent:"center",
-    alignItems:'center',
-    marginVertical:10
+  img: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 10
   }
 });
 
