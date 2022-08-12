f6e89643af1ab634910e2e0c8d287d69619acd54
Rana Shahzaib <shahzaibali1022@yahoo.com>
2022-08-12 19:22:24 +0500

    Batch empty screens

---
+++ modules/screen-donate-card-details/index.js
@@ -0,0 +1,30 @@
+import React, { useRef, useContext, useEffect, useState } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  ImageBackground
+} from "react-native";
+
+const DonateCardDetails = () => {
+
+  return (
+    <View style={styles.heading}>
+      <Text>Donate Card Details</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  heading: {
+    margin: 20,
+    flex: 1,
+    justifyContent: "space-around"
+  }
+});
+
+export default {
+  title: "DonateCardDetails",
+  navigator: DonateCardDetails
+};
---
+++ modules/screen-sponsor-details/index.js
@@ -0,0 +1,30 @@
+import React, { useRef, useContext, useEffect, useState } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  ImageBackground
+} from "react-native";
+
+const SponsorDetails = () => {
+
+  return (
+    <View style={styles.heading}>
+      <Text>Sponsor Details</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  heading: {
+    margin: 20,
+    flex: 1,
+    justifyContent: "space-around"
+  }
+});
+
+export default {
+  title: "SponsorDetails",
+  navigator: SponsorDetails
+};
