6973ea3114002044745deb0a224ffeec51ac1884
saad-abid-crowdbotics <76822297+saad-abid-crowdbotics@users.noreply.github.com>
2022-08-30 15:03:12 +0500

    React native 2fa module (#401)
    * PLAT-7015: Django Modules as Python Packages (#356)
    
    * PLAT-7015: Django modules as Python Packages
    
    - Port to the Python Package structure:
      - django-articles
      - django-push-notifications
      - django-social-auth
      - camera
      - payments
      - privacy-policy
      - terms-and-conditions to package structure
    - Update modules.json
    - Update scripts/parse.js
    - Documentation
    
    Co-authored-by: Daniel Sousa <daniel.s@crowdbotics.com>
    
    * yarn run dist
    
    Co-authored-by: Daniel Sousa <daniel.s@crowdbotics.com>
    
    * Update payments module (#375)
    
    - Add default value to stripe.api_key to fix operations while is not
      yet configured
    
    * [screens] Signin screen (#231)
    
    * Login screen (#232)
    
    * login screen
    
    * yarn lint
    
    Co-authored-by: Daniel <daniel.s@crowdbotics.com>
    
    * add fastlane scaffold metadata and screenshots (#370)
    
    * PLAT-7441 send android build version with webhook (#360)
    
    * remove compare_url and add build_version on webhook callback
    
    * update lock file
    
    * update version_code to use build_num instead of gradle file
    
    * fix conflicts
    
    * update lock file
    
    Co-authored-by: Dan DeFelippi <dan@driverdan.com>
    Co-authored-by: Daniel Sousa <daniel.s@crowdbotics.com>
    
    * Fix: Missing root props on scaffold (#384)
    
    * Fix: add missing props to route component
    
    * Run: yarn raw and yarn cookie
    
    * use action-eslint fork with .eslintignore support
    
    * set ref for danielsousaio/action-eslint
    
    * use full commit sha
    
    * use master ref
    
    * use crowdbotics/action-eslint fork
    
    transfered ownership of the repo
    
    Co-authored-by: Daniel <daniel.s@crowdbotics.com>
    
    * React native 2fa module
    
    * README.md updated
    
    * fixed local imports and updated READEM.md
    
    Co-authored-by: Rodrigo <matematica.a3k@gmail.com>
    Co-authored-by: Daniel Sousa <daniel.s@crowdbotics.com>
    Co-authored-by: Rodrigo <rodrigo@crowdbotics.com>
    Co-authored-by: Muhammad Abdullah Abid <abid.abdullah@crowdbotics.com>
    Co-authored-by: Saurav Gharti <sp.gharti@gmail.com>
    Co-authored-by: Dan DeFelippi <dan@driverdan.com>
    Co-authored-by: Aline Tavares <alaisgomes@gmail.com>

---
+++ modules/react-native-2fa/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigator from "./navigator";
+
+const TwoFactorAuthentication = () => {
+  return (
+    <Navigator />
+  );
+};
+
+export default {
+  title: "2FA",
+  navigator: TwoFactorAuthentication
+};
