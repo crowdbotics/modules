ea830395f3efd1834a8327d5f0fdbe55e37785c8
Saad Bin Abid <saad.abid@crowdbotics.com>
2022-12-07 13:23:46 +0500

    lint fixed

--- modules/screen-onboarding-3/index.js
+++ modules/screen-onboarding-3/index.js
@@ -9,8 +9,8 @@ const Onboarding = ({ navigation }) => {
     <View style={styles.container}>
       <TouchableOpacity style={[styles.prev]}>
       <Image source={require(
-          // @ts-ignore
-          "./assets/prev.png")} style={styles.prevImg} />
+        // @ts-ignore
+        "./assets/prev.png")} style={styles.prevImg} />
         <Text style={styles.prevText}>Prev</Text>
       </TouchableOpacity>
       <Image
@@ -19,7 +19,7 @@ const Onboarding = ({ navigation }) => {
         source={require("./assets/welcome.png")}
       />
       <View style={styles.textContainer}>
-        <Text style={styles.title}>Find your trusted{'\n'}Doctor</Text>
+        <Text style={styles.title}>Find your trusted{"\n"}Doctor</Text>
         <Text style={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. At habitant metus, enim varius in. Morbi diam vel varius cras. </Text>
         <Image source={require(
           // @ts-ignore
@@ -48,14 +48,14 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     position: "absolute",
-    bottom: '7%',
-    width: '100%'
+    bottom: "7%",
+    width: "100%"
   },
   background: {
     width: 370,
     height: 320,
-    left: '15%',
-    resizeMode: "contain",
+    left: "15%",
+    resizeMode: "contain"
   },
   text: { color: "#fff", fontSize: 22, marginRight: 10, textAlign: "center" },
   next: { width: 14.17, height: 11.25, resizeMode: "contain", marginTop: 3 },
@@ -63,21 +63,20 @@ const styles = StyleSheet.create({
   textContainer: {
     justifyContent: "center",
     alignItems: "center",
-    marginTop: '7%'
+    marginTop: "7%"
   },
   title: { fontSize: 24, color: "#FFF", textAlign: "center", lineHeight: 32 },
-  description: { fontSize: 14, color: "#FFF", textAlign: "center", lineHeight: 24, paddingHorizontal: '15%', opacity: 0.7, marginVertical: "2%" },
+  description: { fontSize: 14, color: "#FFF", textAlign: "center", lineHeight: 24, paddingHorizontal: "15%", opacity: 0.7, marginVertical: "2%" },
   prev: {
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
     position: "absolute",
-    top: '5%',
-    width: '100%'
+    top: "5%",
+    width: "100%"
   },
-  prevImg:{ width: 20, height: 18, resizeMode: "contain", marginTop: 3 },
-  prevText:{fontSize: 16, color: "#000", opacity: 0.2, marginLeft: 7}
+  prevImg: { width: 20, height: 18, resizeMode: "contain", marginTop: 3 },
+  prevText: { fontSize: 16, color: "#000", opacity: 0.2, marginLeft: 7 }
 });
 
 export default Onboarding;
-
