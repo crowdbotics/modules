25f535b9a378cdebce684cff86d1404e95e7e3c8
Saad Bin Abid <saad.abid@crowdbotics.com>
2022-12-08 13:23:01 +0500

    register shofipy booking in admin

--- modules/django-booking/django_booking/booking/admin.py
+++ modules/django-booking/django_booking/booking/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Booking, BookingDetail, BookingPenalty, BookingPlan
+from .models import Booking, BookingDetail, BookingPenalty, BookingPlan, ShopifyBooking
 
 admin.site.register(Booking)
 
@@ -8,3 +8,7 @@ admin.site.register(BookingPlan)
 admin.site.register(BookingPenalty)
 
 admin.site.register(BookingDetail)
+
+admin.site.register(ShopifyBooking)
+
+
