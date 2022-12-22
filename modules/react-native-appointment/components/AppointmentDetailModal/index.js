882e41f34cfc030efc83a134844ebae2855c99d4
Saad Bin Abid <saad.abid@crowdbotics.com>
2022-12-22 17:19:33 +0500

    add meeting url

--- modules/react-native-appointment/components/AppointmentDetailModal/index.js
+++ modules/react-native-appointment/components/AppointmentDetailModal/index.js
@@ -1,9 +1,18 @@
 import moment from "moment";
 import React from "react";
-import { Modal, StyleSheet, View, Text, ScrollView } from "react-native";
+import { Modal, StyleSheet, View, Text, ScrollView, TouchableOpacity, Linking, Alert } from "react-native";
 import Button from "../Button";
 
 const AppointmentModal = ({ modalItem, setModalVisible, modalVisible }) => {
+  const handleOpenLink = async (link) => {
+    const supported = await Linking.canOpenURL(link);
+    if (supported) {
+      Linking.openURL(link);
+    } else {
+      Alert.alert("Alert", `Unsupported URL: ${link}`);
+    }
+  };
+
   return (
     <Modal
       animationType="slide"
@@ -12,31 +21,36 @@ const AppointmentModal = ({ modalItem, setModalVisible, modalVisible }) => {
     >
       <View style={styles.modalContainer}>
         <Text style={styles.modalHeaderText}>Appointment details</Text>
-        <Text style={styles.modalText}>Title: {modalItem.summary}</Text>
-        <Text style={styles.modalText}>Status: {modalItem.status}</Text>
-        <Text style={styles.modalText}>Location: {modalItem.location}</Text>
-        <Text style={styles.modalText}>Start time: {("start" in modalItem) ? moment(new Date(modalItem.start.dateTime)).format("YYYY-MM-DD HH:mm A") : ""}</Text>
-        <Text style={styles.modalText}>End time: {("end" in modalItem) ? moment(new Date(modalItem.end.dateTime)).format("YYYY-MM-DD HH:mm A") : ""}</Text>
+        <ScrollView>
+          <Text style={styles.modalText}>Title: {modalItem.summary}</Text>
+          <Text style={styles.modalText}>Status: {modalItem.status}</Text>
+          <Text style={styles.modalText}>Location: {modalItem.location}</Text>
+          <TouchableOpacity onPress={() => handleOpenLink(modalItem.hangoutLink)}>
+            <Text style={styles.modalText}>Meeting: <Text style={styles.textDecorationLine}>{modalItem.hangoutLink}</Text></Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => handleOpenLink(modalItem.htmlLink)}>
+            <Text style={styles.modalText}>Calendar: <Text style={styles.textDecorationLine}>{modalItem.htmlLink}</Text></Text>
+          </TouchableOpacity>
+          <Text style={styles.modalText}>Start time: {("start" in modalItem) ? moment(new Date(modalItem.start.dateTime)).format("YYYY-MM-DD HH:mm A") : ""}</Text>
+          <Text style={styles.modalText}>End time: {("end" in modalItem) ? moment(new Date(modalItem.end.dateTime)).format("YYYY-MM-DD HH:mm A") : ""}</Text>
 
-        <View style={styles.attendeeContainer}>
-          <ScrollView>
-          <Text style={styles.text}>Organizer:</Text>
-          <View style={styles.attendee}>
-                <Text style={styles.attendeeText}>{modalItem?.creator?.email}</Text>
+          <View style={styles.attendeeContainer}>
+            <Text style={styles.text}>Organizer:</Text>
+            <View style={styles.attendee}>
+              <Text style={styles.attendeeText}>{modalItem?.creator?.email}</Text>
+            </View>
+            <Text style={[styles.text, styles.mt]}>Participants:</Text>
+            {
+              modalItem?.attendees
+                ? modalItem?.attendees?.map((attendee, index) =>
+                  <View style={styles.attendee} key={index}>
+                    {!attendee?.organizer && <Text style={styles.attendeeText}>{attendee?.organizer ? "" : attendee.email}</Text>}
+                  </View>
+                )
+                : <Text style={styles.noAttendee}>No Participants!</Text>
+            }
           </View>
-          <Text style={[styles.text, styles.mt]}>Participants:</Text>
-          {
-             modalItem?.attendees
-               ? modalItem?.attendees?.map((attendee, index) =>
-              <View style={styles.attendee} key={index}>
-                {!attendee?.organizer && <Text style={styles.attendeeText}>{attendee?.organizer ? "" : attendee.email}</Text>}
-              </View>
-               )
-               : <Text style={styles.noAttendee}>No Participants!</Text>
-          }
-          </ScrollView>
-        </View>
-
+        </ScrollView>
         <View style={styles.modalActionButton}>
           <Button height={40} onPress={() => setModalVisible(false)} style={styles.hide}>
             Cancel
@@ -48,7 +62,7 @@ const AppointmentModal = ({ modalItem, setModalVisible, modalVisible }) => {
 };
 
 const styles = StyleSheet.create({
-  modalContainer: { minHeight: "50%", width: "95%", alignSelf: "center", backgroundColor: "#FFF", display: "flex", justifyContent: "center", marginTop: "15%", borderRadius: 10, padding: 20, shadowColor: "gray", elevation: 15 },
+  modalContainer: { minHeight: "50%", maxHeight: "85%", width: "95%", alignSelf: "center", backgroundColor: "#FFF", display: "flex", justifyContent: "center", marginTop: "15%", borderRadius: 10, padding: 20, shadowColor: "gray", elevation: 15 },
   hide: { marginTop: "30%", alignSelf: "center" },
   modalText: { fontSize: 16, padding: 15, marginVertical: 4, borderWidth: 1, borderColor: "#CCCCCC", borderRadius: 4, borderLeftWidth: 5, borderLeftColor: "#000" },
   attendeeContainer: { paddingVertical: 10, paddingHorizontal: 10, marginVertical: 4, borderWidth: 1, borderColor: "#CCCCCC", borderRadius: 4, borderLeftWidth: 5, borderLeftColor: "#000", height: "30%" },
@@ -71,7 +85,8 @@ const styles = StyleSheet.create({
   organizer: { color: "#3bae61", fontSize: 12 },
   mt: { marginTop: 5 },
   text: { marginBottom: 5 },
-  noAttendee: { textAlign: "center", fontSize: 12, color: "#a59b9b" }
+  noAttendee: { textAlign: "center", fontSize: 12, color: "#a59b9b" },
+  textDecorationLine: { textDecorationLine: "underline" }
 });
 
 export default AppointmentModal;
