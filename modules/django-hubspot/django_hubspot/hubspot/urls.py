2eaad40369a7d3826af0a9044985faf21c9af641
Saad Bin Abid <saad.abid@crowdbotics.com>
2022-12-23 21:24:55 +0500

    Created meetingContactsAssociation API, and dealsContact API

--- modules/django-hubspot/django_hubspot/hubspot/urls.py
+++ modules/django-hubspot/django_hubspot/hubspot/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .viewsets import (AccessTokenViewSet, DealsListViewSet, CreateDealViewSet, RemoveDealViewSet, SingleDealViewSet, WebHookViewSet)
+from .viewsets import (AccessTokenViewSet, DealsListViewSet, CreateDealViewSet, RemoveDealViewSet, SingleDealViewSet,
+                       WebHookViewSet, MeetingToContactsListViewSet, ContactToDealsListViewSet)
 
 urlpatterns = [
     path('access/token/', AccessTokenViewSet.as_view()),
@@ -7,5 +8,7 @@ urlpatterns = [
     path('deals/create/', CreateDealViewSet.as_view()),
     path('deals/remove/', RemoveDealViewSet.as_view()),
     path('deals/single/', SingleDealViewSet.as_view()),
+    path('contact/deals/list/', ContactToDealsListViewSet.as_view()),
+    path('meeting/contacts/list/', MeetingToContactsListViewSet.as_view()),
     path('webhook/', WebHookViewSet.as_view()),
 ]
--- modules/django-hubspot/django_hubspot/hubspot/viewsets.py
+++ modules/django-hubspot/django_hubspot/hubspot/viewsets.py
@@ -5,6 +5,55 @@ import requests
 from demo.settings import BASE_URL, GRANT_TYPE, REDIRECT_URL, CLIENT_ID, CLIENT_SECRET
 
 
+class HubspotAPIView(APIView):
+
+    def get_payload(self):
+        return {}
+
+    def get_params(self):
+        return {}
+
+    def get_url(self):
+        return ""
+
+    def get(self, request, *args, **kwargs):
+        try:
+            headers = {
+                "Authorization": request.META.get('HTTP_AUTHORIZATION')
+            }
+            response = requests.get(self.get_url(), params=self.get_params(), headers=headers)
+            response.raise_for_status()
+            load = response.json()
+            return Response(load, status=status.HTTP_200_OK)
+        except requests.exceptions.RequestException as e:
+            return Response(e.response, status=e.response.status_code)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            headers = {
+                "Authorization": request.META.get('HTTP_AUTHORIZATION')
+            }
+            response = requests.post(self.get_url(), json=self.get_payload(), params=self.get_params(),
+                                     headers=headers)
+            response.raise_for_status()
+            load = response.json()
+            return Response(load, status=status.HTTP_200_OK)
+        except requests.exceptions.RequestException as e:
+            return Response(e.response, status=e.response.status_code)
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            headers = {
+                "Authorization": request.META.get('HTTP_AUTHORIZATION')
+            }
+            response = requests.delete(self.get_url(), json=self.get_payload(), params=self.get_params(),
+                                       headers=headers)
+            response.raise_for_status()
+            return Response(response, status=status.HTTP_204_NO_CONTENT)
+        except requests.exceptions.RequestException as e:
+            return Response(e.response, status=e.response.status_code)
+
+
 class AccessTokenViewSet(APIView):
 
     def post(self, request, *args, **kwargs):
@@ -24,65 +73,42 @@ class AccessTokenViewSet(APIView):
             return Response(e.response, status=e.response.status_code)
 
 
-class DealsListViewSet(APIView):
+class DealsListViewSet(HubspotAPIView):
 
-    def get(self, request, *args, **kwargs):
-        try:
-            header = {
-                "Authorization": request.META.get("HTTP_AUTHORIZATION"),
-            }
-            response = requests.get(BASE_URL + 'crm/v4/objects/deals/', headers=header)
-            response.raise_for_status()
-            load = response.json()
-            return Response(load, status=status.HTTP_200_OK)
-        except requests.exceptions.RequestException as e:
-            return Response(e.response, status=e.response.status_code)
+    def get_url(self):
+        return f"{BASE_URL}crm/v4/objects/deals/"
 
 
-class CreateDealViewSet(APIView):
+class CreateDealViewSet(HubspotAPIView):
+    def get_url(self):
+        return f"{BASE_URL}crm/v4/objects/deals/"
 
-    def post(self, request, *args, **kwargs):
-        try:
-            header = {
-                "Authorization": request.META.get("HTTP_AUTHORIZATION"),
-            }
-            response = requests.post(BASE_URL + 'crm/v4/objects/deals/', json=request.data, headers=header)
-            response.raise_for_status()
-            load = response.json()
-            return Response(load, status=status.HTTP_201_CREATED)
-        except requests.exceptions.RequestException as e:
-            return Response(e.response, status=e.response.status_code)
+    def get_payload(self):
+        return self.request.data
 
 
-class RemoveDealViewSet(APIView):
+class RemoveDealViewSet(HubspotAPIView):
 
-    def delete(self, request, *args, **kwargs):
-        try:
-            header = {
-                "Authorization": request.META.get("HTTP_AUTHORIZATION"),
-            }
-            response = requests.delete(BASE_URL + 'crm/v4/objects/deals/' + request.data.get('id'),
-                                       headers=header)
-            response.raise_for_status()
-            return Response(response, status=status.HTTP_204_NO_CONTENT)
-        except requests.exceptions.RequestException as e:
-            return Response(e.response, status=e.response.status_code)
+    def get_url(self):
+        return f"{BASE_URL}'crm/v4/objects/deals/{self.request.data.get('id')}"
 
 
-class SingleDealViewSet(APIView):
+class SingleDealViewSet(HubspotAPIView):
 
-    def get(self, request, *args, **kwargs):
-        try:
-            header = {
-                "Authorization": request.META.get("HTTP_AUTHORIZATION"),
-            }
-            response = requests.get(BASE_URL + 'crm/v4/objects/deals/' + request.data.get('id'),
-                                    headers=header)
-            response.raise_for_status()
-            load = response.json()
-            return Response(load, status=status.HTTP_200_OK)
-        except requests.exceptions.RequestException as e:
-            return Response(e.response, status=e.response.status_code)
+    def get_url(self):
+        return f"{BASE_URL}crm/v4/objects/deals/{self.request.data.get('id')}"
+
+
+class MeetingToContactsListViewSet(HubspotAPIView):
+
+    def get_url(self):
+        return f"{BASE_URL}crm/v4/objects/meetings/{self.request.data.get('meetingId')}/associations/contacts/"
+
+
+class ContactToDealsListViewSet(HubspotAPIView):
+
+    def get_url(self):
+        return f"{BASE_URL}crm/v4/objects/contacts/{self.request.data.get('contactId')}/associations/deals/"
 
 
 class WebHookViewSet(APIView):
@@ -90,4 +116,3 @@ class WebHookViewSet(APIView):
     def post(self, request, *args, **kwargs):
         print("Event Triggered: ", request.data)
         return Response(request.data, status=status.HTTP_200_OK)
-        
