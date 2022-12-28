bc2042f59b7383a25c9a18a6baee771e100ce107
Saad Bin Abid <saad.abid@crowdbotics.com>
2022-12-28 21:01:47 +0500

    updated code comments

--- modules/django-hubspot/django_hubspot/hubspot/viewsets.py
+++ modules/django-hubspot/django_hubspot/hubspot/viewsets.py
@@ -1,4 +1,6 @@
 import os
+
+from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -45,8 +47,8 @@ class HubspotViewSet(viewsets.GenericViewSet):
     def remove_deal(self, request):
         """
         To delete a deal
-        :param str(dealId):  HubSpot deal id to delete the deal.
-        :return: no content.      
+        :param str dealId:  HubSpot deal id to delete the deal.
+        :return: Removes the deal from hubspot and returns no content.      
         """
         response = self.hubspot_service.remove_deal(request.data.get('dealId'))
         return Response(data=response.get("data"), status=response.get("status_code"))
@@ -55,8 +57,8 @@ class HubspotViewSet(viewsets.GenericViewSet):
     def single_deal(self, request):
         """
         To retrieve a deal
-        :param str(dealId):  HubSpot deal id to retrieve the deal.
-        :return: Returns a deal with properties.       
+        :param str dealId:  HubSpot deal id to retrieve the deal.
+        :return: Returns a deal object with properties.       
         """
         response = self.hubspot_service.single_deal(request.data.get('dealId'))
         return Response(data=response.get("data"), status=response.get("status_code"))
@@ -83,7 +85,7 @@ class HubspotViewSet(viewsets.GenericViewSet):
             hubspot_owner_id
             subject
             content
-        :return: Returns a newly created ticket.
+        :return: Returns a newly created ticket object.
         """
         response = self.hubspot_service.create_ticket(request.data)
         return Response(data=response.get("data"), status=response.get("status_code"))
@@ -92,8 +94,8 @@ class HubspotViewSet(viewsets.GenericViewSet):
     def remove_ticket(self, request):
         """
         To delete a ticket
-        :param str(ticketId):  HubSpot ticket id to delete the ticket.
-        :return: no content.      
+        :param str ticketId:  HubSpot ticket id to delete the ticket.
+        :return: Removes the ticket from hubspot and returns no content.      
         """
         response = self.hubspot_service.remove_ticket(request.data)
         return Response(data=response.get("data"), status=response.get("status_code"))
@@ -102,8 +104,8 @@ class HubspotViewSet(viewsets.GenericViewSet):
     def single_ticket(self, request):
         """
         To retrieve a ticket
-        :param str(ticketId):  HubSpot ticket id to retrieve the ticket.
-        :return: Returns a ticket with properties.       
+        :param str ticketId:  HubSpot ticket id to retrieve the ticket.
+        :return: Returns a ticket object containing ticket detail.       
         """
         response = self.hubspot_service.single_ticket(request.data)
         return Response(data=response.get("data"), status=response.get("status_code"))
@@ -132,9 +134,8 @@ class HubspotViewSet(viewsets.GenericViewSet):
     def ticket_association_list(self, request):
         """
         To get a ticket associated with other CRM objects
-        :params
-            ticketId
-            toObjectType
+        :param str ticketId: HubSpot ticket id associated with a CRM object
+        :param str toObjectType: Type of the CRM object to with ticket is being associated
         :return: Returns a ticket associated with other HubSpot objects.
         """
         response = self.hubspot_service.ticket_association_list(request.data)
@@ -144,9 +145,7 @@ class HubspotViewSet(viewsets.GenericViewSet):
     def contact_deals_association_list(self, request):
         """
         To get a contact associated with other HubSpot deals
-        :params
-            ticketId
-            toObjectType
+        :param str contactId: HubSpot contact id associated with a CRM deals object
         :return: Returns a contact associated with other HubSpot deals.
         """
         response = self.hubspot_service.contact_deals_association_list(request.data)
@@ -156,9 +155,7 @@ class HubspotViewSet(viewsets.GenericViewSet):
     def meeting_contacts_association_list(self, request):
         """
         To get a meeting associated with other HubSpot contacts.
-        :params
-        ticketId
-        toObjectType
+        :param str meetingId: HubSpot meeting id associated with a CRM contact object
         :return: Returns a meeting associated with other HubSpot contacts.
         """
         response = self.hubspot_service.meeting_contact_association_list(request.data)
@@ -192,4 +189,4 @@ class HubspotViewSet(viewsets.GenericViewSet):
         subscribed hubspot events triggered.
         """
         response = self.hubspot_service.webhook(request.data)
-        return Response(data=response.get("data"), status=response.get("status_code"))
+        return Response(data=response, status=status.HTTP_200_OK)
