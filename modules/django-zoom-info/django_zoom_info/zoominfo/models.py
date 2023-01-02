da839076119512f3c86fcad18c818af0a0ec862e
cbshoaib <120275623+cbshoaib@users.noreply.github.com>
2023-01-03 00:25:42 +0500

    ZoomInfo module (#761)
    * create token , searh, enrich and bulk apis
    
    * create api to save enrich data to Database
    
    * add migration file
    
    * added preview image

---
+++ modules/django-zoom-info/django_zoom_info/zoominfo/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+
+
+class Person(models.Model):
+    firstName = models.CharField(max_length=100, null=False, blank=True)
+    middleName = models.CharField(max_length=100, null=True, blank=True)
+    lastName = models.CharField(max_length=100, null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
+    jobtitle = models.CharField(max_length=100, null=True, blank=True)
+    company = models.ForeignKey("Company", on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.firstName)
+
+    class Meta:
+        verbose_name_plural = "Person"
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
+    website = models.CharField(max_length=100, null=True, blank=True)
+    revenue = models.CharField(max_length=100, null=True, blank=True)
+    revenueRange = models.CharField(max_length=100, null=True, blank=True)
+    employeeCount = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name_plural = "Company"
+
+
+class Education(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, blank=True, null=True)
+    school = models.CharField(max_length=100, null=True, blank=True)
+    educationDegree = models.CharField(max_length=100, null=True, blank=True)
+    areaOfStudy = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.school)
+
+    class Meta:
+        verbose_name_plural = "Education"
+
+
+class SocialMedia(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, blank=True, null=True)
+    type = models.CharField(max_length=100, null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.type)
+
+    class Meta:
+        verbose_name_plural = "SocialMedia"
