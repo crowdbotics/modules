106bc44d8c1d845bf81964448c32b2219653bcfc
Shoaib Amjad <muhammad.shoaib@crowdbotics.com>
2023-01-03 17:25:44 +0500

    created apis and endpoints for posts, sites, users and comments

--- modules/django-wordpress/django_wordpress/wordpress/services/WordpressService.py
+++ modules/django-wordpress/django_wordpress/wordpress/services/WordpressService.py
@@ -31,7 +31,7 @@ class WordpressBase:
     def _api_call(self, request_type, url, access_token=None, payload=None, params=None):
         try:
             headers = {"Authorization": f"Bearer {access_token}"}
-            response = requests.request(request_type, url, headers=headers, data=payload)
+            response = requests.request(request_type, url, headers=headers, data=payload, params=params)
             response.raise_for_status()
             if response.status_code == 204:
                 return {"data": response, "status_code": response.status_code, "success": True}
@@ -77,7 +77,7 @@ class WordpressService(WordpressBase):
 
     def get_multiple_posts(self, access_token, params=None):
         try:
-            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/posts/{post_id}"
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/posts/"
             response = self._api_call(request_type='GET', url=url,
                                       access_token=access_token,
                                       params=params
@@ -86,9 +86,19 @@ class WordpressService(WordpressBase):
         except Exception as e:
             return e
 
-    def delete_single_post(self, access_token, post_id, request_body=None):
+    def delete_single_post(self, access_token, post_id):
         try:
             url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/posts/{post_id}/delete"
+            response = self._api_call(request_type='POST', url=url,
+                                      access_token=access_token,
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def delete_multiple_post(self, access_token, request_body):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/posts/delete"
             response = self._api_call(request_type='POST', url=url,
                                       access_token=access_token,
                                       payload=request_body
@@ -108,4 +118,179 @@ class WordpressService(WordpressBase):
         except Exception as e:
             return e
 
-        
+    def get_list_of_likes(self, access_token, post_id):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/posts/{post_id}/likes/"
+            response = self._api_call(request_type='GET', url=url,
+                                      access_token=access_token,
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def like_post(self, access_token, post_id):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/posts/{post_id}/likes/new"
+            response = self._api_call(request_type='POST', url=url,
+                                      access_token=access_token,
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def unlike_post(self, access_token, post_id):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/posts/{post_id}/likes/mine/delete"
+            response = self._api_call(request_type='POST', url=url,
+                                      access_token=access_token,
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def get_list_of_post_subscribers(self, access_token, post_id):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/posts/{post_id}/subscribers/"
+            response = self._api_call(request_type='GET', url=url,
+                                      access_token=access_token,
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def get_multiple_users(self, access_token, params=None):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/users"
+            response = self._api_call(request_type='GET', url=url,
+                                      access_token=access_token,
+                                      params=params
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def update_user_details(self, access_token, user_id, request_body):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/users/{user_id}"
+            response = self._api_call(request_type='POST', url=url,
+                                      access_token=access_token,
+                                      payload=request_body
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def delete_user(self, access_token, user_id):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/users/{user_id}/delete"
+            response = self._api_call(request_type='POST', url=url,
+                                      access_token=access_token,
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def get_rendered_shortcode_for_site(self, access_token):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/users"
+            response = self._api_call(request_type='GET', url=url,
+                                      access_token=access_token,
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def get_active_inactive_widgets(self, access_token):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/widgets"
+            response = self._api_call(request_type='GET', url=url,
+                                      access_token=access_token,
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def activate_widget(self, access_token, request_body=None):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/widgets/new"
+            response = self._api_call(request_type='POST', url=url,
+                                      access_token=access_token,
+                                      payload=request_body
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def deactivate_widget(self, access_token, widget_id):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/widgets/{widget_id}/delete"
+            response = self._api_call(request_type='POST', url=url,
+                                      access_token=access_token,
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def get_single_comment(self, access_token, comment_id):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/comments/{comment_id}"
+            response = self._api_call(request_type='GET', url=url,
+                                      access_token=access_token,
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def edit_comment(self, access_token, comment_id, request_data):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/comments/{comment_id}"
+            response = self._api_call(request_type='POST', url=url,
+                                      access_token=access_token,
+                                      params=request_data
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def delete_comment(self, access_token, comment_id):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/comments/{comment_id}/delete"
+            response = self._api_call(request_type='POST', url=url,
+                                      access_token=access_token,
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def create_comment_on_post(self, access_token, request_data, post_id):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/posts/{post_id}/replies/new"
+            response = self._api_call(request_type='POST', url=url,
+                                      access_token=access_token,
+                                      payload=request_data
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def like_comment(self, access_token, comment_id):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/comments/{comment_id}/likes/new"
+            response = self._api_call(request_type='POST', url=url,
+                                      access_token=access_token,
+                                      )
+            return response
+        except Exception as e:
+            return e
+
+    def unlike_comment(self, access_token, comment_id):
+        try:
+            url = f"{self.WORDPRESS_BASE_URL}/rest/v1.1/sites/{self.WORDPRESS_DOMAIN}/comments/{comment_id}" \
+                  f"/likes/mine/delete"
+            response = self._api_call(request_type='POST', url=url,
+                                      access_token=access_token,
+                                      )
+            return response
+        except Exception as e:
+            return e
+
--- modules/django-wordpress/django_wordpress/wordpress/viewsets.py
+++ modules/django-wordpress/django_wordpress/wordpress/viewsets.py
@@ -2,24 +2,28 @@ import os
 
 from rest_framework.decorators import action
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.response import Response
+from rest_framework import status
 
 from .services.WordpressService import WordpressService
 
 
 class WordpressViewSet(GenericViewSet):
     wordpress_service = WordpressService(base_url=os.getenv('WORDPRESS_BASE_URL', ""),
-                                         client_id=os.getenv('WORDPRESS_BASE_URL', ""),
-                                         client_secrets=os.getenv('WORDPRESS_BASE_URL', ""),
+                                         client_id=os.getenv('WORDPRESS_CLIENT_ID', ""),
+                                         client_secrets=os.getenv('WORDPRESS_CLIENT_SECRETS', ""),
                                          redirect_url=os.getenv("WORDPRESS_REDIRECT_URL", ""),
                                          wordpress_domain=os.getenv("WORDPRESS_DOMAIN", "")
                                          )
 
     @action(detail=False, methods=['post'], url_path='get-auth-token')
     def get_auth_token(self, request):
-        code = request.META.get('WORDPRESS_CODE')
+        code = request.META.get('HTTP_WORDPRESS_CODE')
         if code:
             response = self.wordpress_service.get_auth_token(wordpress_code=code)
-            return Response(data=response, status=response.get("status_code"))
+            if response:
+                return Response(data=response, status=status.HTTP_200_OK)
+            return Response(data={"error": "Code Expire. Please try again."}, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response({
                 "errorCode": "CODE_NOT_FOUND",
@@ -38,24 +42,287 @@ class WordpressViewSet(GenericViewSet):
                 "message": "Access token not found"
             }, status=status.HTTP_404_BAD_REQUEST)
 
+    @action(detail=True, methods=['post'], url_path='edit-post')
+    def edit_post(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.edit_post(access_token=access_token, request_body=request.data,
+                                                        post_id=pk)
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
 
-# class AuthTest(APIView):
-#     permission_classes = [AllowAny]
-#
-#     def post(self, request, *args, **kwargs):
-#         client_id = '83370'
-#         client_secret = 'HDNIQO5gsTgIm9MJURUkTIeQz28rUcLkonBPZyiL57HYx7Xe69FnerZWGKixaIlk'
-#         uri = 'https://www.crowdbotics.com/'
-#         # url = f"https://public-api.wordpress.com/oauth2/authorize?client_id={client_id}&redirect_uri={uri}&response_type=code"
-#         url = "https://public-api.wordpress.com/oauth2/token"
-#
-#         payload = {
-#             "client_id": client_id,
-#             "client_secret": client_secret,
-#             "redirect_uri": uri,
-#             "code": '7RCcHuic2C',
-#             "grant_type": 'authorization_code'
-#         }
-#
-#         response = requests.post(url=url, data=payload)
-#         return {"data": response.json()}
+    @action(detail=False, methods=['get'], url_path='get-multiple-post')
+    def get_multiple_post(self, request):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            params = request.query_params.dict()
+            response = self.wordpress_service.get_multiple_posts(access_token=access_token, params=params)
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'], url_path='delete-single-post')
+    def delete_single_post(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.delete_single_post(access_token=access_token, post_id=pk)
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=False, methods=['post'], url_path='delete-multiple-post')
+    def delete_multiple_post(self, request):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.delete_multiple_post(access_token=access_token, request_body=request.data)
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'], url_path='restore-post')
+    def restore_post(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.restore_post(access_token=access_token, post_id=pk,
+                                                           request_body=request.data
+                                                           )
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=True, methods=['get'], url_path='get-list-of-likes')
+    def get_list_of_likes(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.get_list_of_likes(access_token=access_token, post_id=pk)
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'], url_path='like-post')
+    def like_post(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.like_post(access_token=access_token, post_id=pk)
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'], url_path='unlike-post')
+    def unlike_post(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.unlike_post(access_token=access_token, post_id=pk)
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=True, methods=['get'], url_path='get-list-of-post-subscribers')
+    def get_list_of_likes(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.get_list_of_post_subscribers(access_token=access_token, post_id=pk)
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=False, methods=['get'], url_path='get-multiple-users')
+    def get_multiple_users(self, request):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            params = request.query_params.dict()
+            response = self.wordpress_service.get_multiple_users(access_token=access_token, params=params)
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'], url_path='update-user-details')
+    def update_user_details(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.update_user_details(access_token=access_token,
+                                                                  user_id=pk,
+                                                                  request_body=request.data)
+
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'], url_path='delete-user')
+    def delete_user(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.delete_user(access_token=access_token,
+                                                          user_id=pk)
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=False, methods=['get'], url_path='delete-user')
+    def get_rendered_shortcode_for_site(self, request):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.get_rendered_shortcode_for_site(access_token=access_token)
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=False, methods=['get'], url_path='get-active-inactive-widgets')
+    def get_active_inactive_widgets(self, request):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.get_active_inactive_widgets(access_token=access_token)
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=False, methods=['post'], url_path='activate_widget')
+    def activate_widget(self, request):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.activate_widget(access_token=access_token, request_body=request.data)
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'], url_path='deactivate_widget')
+    def deactivate_widget(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.deactivate_widget(access_token=access_token, widget_id=pk)
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=True, methods=['get'], url_path='get_single_comment')
+    def get_single_comment(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.get_single_comment(access_token=access_token, comment_id=pk)
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'], url_path='edit_comment')
+    def edit_comment(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.edit_comment(access_token=access_token,
+                                                           comment_id=pk,
+                                                           request_data=request.data
+                                                           )
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'], url_path='delete_comment')
+    def delete_comment(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.delete_comment(access_token=access_token,
+                                                             comment_id=pk,
+                                                             )
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'], url_path='create-comment-on-post')
+    def create_comment_on_post(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.create_comment_on_post(access_token=access_token,
+                                                                     request_data=request.data,
+                                                                     post_id=pk,
+                                                                     )
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'], url_path='like-comment')
+    def like_comment(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.like_comment(access_token=access_token,
+                                                           comment_id=pk
+                                                           )
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'], url_path='unlike-comment')
+    def unlike_comment(self, request, pk):
+        access_token = request.META.get('HTTP_AUTHORIZATION')
+        if access_token:
+            response = self.wordpress_service.unlike_comment(access_token=access_token,
+                                                             comment_id=pk
+                                                             )
+            return Response(data=response, status=response.get("status_code"))
+        else:
+            return Response({
+                "errorCode": "ACCESS_TOKEN_NOT_FOUND",
+                "message": "Access token not found"
+            }, status=status.HTTP_404_BAD_REQUEST)
