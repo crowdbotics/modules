383be06a579f753a94b4933e7ee6415b5f17f883
shoaib0332 <muhammad.shoaib@crowdbotics.com>
2023-01-12 19:32:59 +0500

    Added Insights router

--- modules/django-wordpress/django_wordpress/wordpress/urls.py
+++ modules/django-wordpress/django_wordpress/wordpress/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from .viewsets import WordpressPostsViewSet, WordpressAuthViewSet, WordpressUsersViewSet, WordpressSitesViewSet, \
     WordpressCommentsViewSet, WordpressTaxonomyViewSet, WordpressFollowViewSet, WordpressFreshlyPressedViewSet, \
-    WordpressReaderViewSet, WordpressStatsViewSet, WordpressMenuViewSet
+    WordpressReaderViewSet, WordpressStatsViewSet, WordpressMenuViewSet, WordpressInsightsViewSet
 
 router = DefaultRouter()
 router.register("auth", WordpressAuthViewSet, basename="wordpress_auth")
@@ -17,6 +17,8 @@ router.register("freshly-pressed", WordpressFreshlyPressedViewSet, basename="wor
 router.register("reader", WordpressReaderViewSet, basename="wordpress_reader")
 router.register("stats", WordpressStatsViewSet, basename="wordpress_stats")
 router.register("menu", WordpressMenuViewSet, basename="wordpress_menu")
+router.register("insights", WordpressInsightsViewSet, basename="wordpress_insights")
+
 
 urlpatterns = [
     path("", include(router.urls)),
