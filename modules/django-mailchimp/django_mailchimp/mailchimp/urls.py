60b9e33eb1ef28d1a62eb691abf404557e5bfecf
Shoaib Amjad <muhammad.shoaib@crowdbotics.com>
2023-01-13 19:18:05 +0500

    add campaign-reports router

--- modules/django-mailchimp/django_mailchimp/mailchimp/urls.py
+++ modules/django-mailchimp/django_mailchimp/mailchimp/urls.py
@@ -9,6 +9,7 @@ router = DefaultRouter()
 router.register("audience", MailchimpAudienceViewSet, basename="mailchimp_audience")
 router.register("templates", MailchimpTemplatesViewSet, basename="mailchimp_templates")
 router.register("campaigns", MailchimpCampaignViewSet, basename="mailchimp_campaigns")
+router.register("campaign-reports", MailchimpReportsViewSet, basename="mailchimp_campaign_reports")
 
 urlpatterns = [
     path("", include(router.urls)),
