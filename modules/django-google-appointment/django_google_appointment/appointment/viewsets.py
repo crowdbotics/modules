7b8bb09aff3746db6366d35c8466dc8ee0c8aa47
Saad Bin Abid <saad.abid@crowdbotics.com>
2023-01-24 19:10:08 +0500

    update sync url

--- modules/django-google-appointment/django_google_appointment/appointment/viewsets.py
+++ modules/django-google-appointment/django_google_appointment/appointment/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, status
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -80,7 +81,8 @@ class GoogleAppointmentViewSet(viewsets.GenericViewSet):
         except Exception as e:
             return Response(e.args, status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=False, methods=['get'], url_path='appointment-synced-list', permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=['get'], url_path='appointment-synced-list', permission_classes=[IsAuthenticated],
+            authentication_classes=[SessionAuthentication, TokenAuthentication])
     def synced_appointment_list(self, request):
         serializer = MeetingListSerializer(Meetings.objects.all(), many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
