130d05287a3a8e273781b699605040293cc80d6c
Saad Bin Abid <saad.abid@crowdbotics.com>
2023-03-15 17:30:49 +0500

    created loader component

--- modules/react-native-user-profile/index.js
+++ modules/react-native-user-profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, ScrollView, ActivityIndicator, Text } from "react-native";
+import { View, ScrollView, ActivityIndicator, Text, StyleSheet } from "react-native";
 import { getUserById, slice } from "./store";
 import { useSelector, useDispatch } from "react-redux";
 import { styles, Color } from "./styles";
@@ -35,16 +35,14 @@ export const UserProfile = ({ route, textInputStyle = {}, buttonStyle = {}, butt
     <ScrollView style={styles.container} contentStyle={styles.content}>
       {api.loading === "pending"
         ? (
-          <View>
-            <ActivityIndicator color={Color.steel} />
-          </View>
+          <Loader color={Color.steel} />
           )
         : (
           <View>
             <View>{!user && <Text>No user to display information.</Text>}</View>
             {user && (
               <View>
-                {isEdit ? <EditUser user={user} textInputStyle={textInputStyle} buttonStyle={buttonStyle} buttonTextStyle={buttonTextStyle} avatarStyle={avatarStyle} editContainerStyle={editContainerStyle}/> : <ViewUser user={user} userInfoContainerStyle={userInfoContainerStyle} avatarStyle={avatarStyle}/>}
+                {isEdit ? <EditUser user={user} textInputStyle={textInputStyle} buttonStyle={buttonStyle} buttonTextStyle={buttonTextStyle} avatarStyle={avatarStyle} editContainerStyle={editContainerStyle} /> : <ViewUser user={user} userInfoContainerStyle={userInfoContainerStyle} avatarStyle={avatarStyle} />}
               </View>
             )}
           </View>
@@ -67,3 +65,33 @@ export default {
   navigator: UserProfile,
   slice
 };
+
+const Loader = ({ color }) => {
+  return (
+    <View style={loaderStyles.container}>
+      <View style={loaderStyles.loaderContainer}>
+        <ActivityIndicator color={color} />
+      </View>
+    </View>
+  );
+};
+const loaderStyles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 9999
+  },
+  loaderContainer: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5F5F5",
+    shadowColor: "#000",
+    elevation: 3
+  }
+});
