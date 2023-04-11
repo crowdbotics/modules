a74567bc8509a2c12b3057fe7a4e51ad2723887a
Saad Bin Abid <saad.abid@crowdbotics.com>
2023-04-11 12:53:52 +0500

    Removed unused api's from store

--- modules/react-native-2fa/store/index.js
+++ modules/react-native-2fa/store/index.js
@@ -1,18 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { api } from "./api";
 
-export const getUser = createAsyncThunk(
-  "2fa/getUser",
-  async (payload, { rejectWithValue }) => {
-    try {
-      const response = await api.getUser(payload);
-      return response.data;
-    } catch (error) {
-      return rejectWithValue(error.response.data);
-    }
-  }
-);
-
 export const getCode = createAsyncThunk(
   "2fa/getCode",
   async (payload, { rejectWithValue }) => {
@@ -49,33 +37,8 @@ export const verifyCode = createAsyncThunk(
   }
 );
 
-export const verify2FA = createAsyncThunk(
-  "2fa/verify2FA",
-  async (payload, { rejectWithValue }) => {
-    try {
-      const response = await api.verify2FA(payload);
-      return response.data;
-    } catch (error) {
-      return rejectWithValue(error.response.data);
-    }
-  }
-);
-
-export const set2faMethod = createAsyncThunk(
-  "2fa/set2faMethod",
-  async (payload, { rejectWithValue }) => {
-    try {
-      const response = await api.set2faMethod(payload);
-      return response.data;
-    } catch (error) {
-      return rejectWithValue(error.response.data);
-    }
-  }
-);
-
 const initialState = {
   token: null,
-  user: {},
   otpCode: {},
   api: { loading: "idle", error: null }
 };
@@ -85,24 +48,6 @@ export const slice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(getUser.pending, (state) => {
-        if (state.api.loading === "idle") {
-          state.api.loading = "pending";
-          state.api.error = null;
-        }
-      })
-      .addCase(getUser.fulfilled, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.user = action.payload;
-          state.api.loading = "idle";
-        }
-      })
-      .addCase(getUser.rejected, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.api.error = action.error;
-          state.api.loading = "idle";
-        }
-      })
       .addCase(getCode.pending, (state) => {
         if (state.api.loading === "idle") {
           state.api.loading = "pending";
@@ -150,40 +95,6 @@ export const slice = createSlice({
         }
       })
       .addCase(verifyCode.rejected, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.api.error = action.error;
-          state.api.loading = "idle";
-        }
-      })
-      .addCase(verify2FA.pending, (state) => {
-        if (state.api.loading === "idle") {
-          state.api.loading = "pending";
-          state.api.error = null;
-        }
-      })
-      .addCase(verify2FA.fulfilled, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.api.loading = "idle";
-        }
-      })
-      .addCase(verify2FA.rejected, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.api.error = action.error;
-          state.api.loading = "idle";
-        }
-      })
-      .addCase(set2faMethod.pending, (state) => {
-        if (state.api.loading === "idle") {
-          state.api.loading = "pending";
-          state.api.error = null;
-        }
-      })
-      .addCase(set2faMethod.fulfilled, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.api.loading = "idle";
-        }
-      })
-      .addCase(set2faMethod.rejected, (state, action) => {
         if (state.api.loading === "pending") {
           state.api.error = action.error;
           state.api.loading = "idle";
