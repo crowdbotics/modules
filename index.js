1da8d60c9aed9c0aa169344ecd8ce2a76c9c3c07
Daniel <daniel.s@crowdbotics.com>
2023-05-01 17:57:17 +0100

    add scaffold upgrade prompt

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 #!/usr/bin/env node
+import path from "node:path";
+import inquirer from "inquirer";
+import { spawn } from "node:child_process";
+import { fileURLToPath } from "node:url";
 
-console.log("hello");
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const actions = {
+  ["Quit"]: () => process.exit(1),
+  ["Upgrade my scaffold"]: () => {
+    console.log("Stashing local changes");
+    spawn("git", ["stash", "-u"], {
+      cwd: __dirname,
+      stdio: "inherit"
+    });
+
+    console.log("scaffold upgrade started");
+    process.exit(1);
+  }
+};
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "What do you want to do?",
+      choices: ["Quit", "Upgrade my scaffold"]
+    }
+  ])
+  .then((answers) => {
+    actions[answers.action]();
+    process.exit(1);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+      console.error("Your environment isn't compatible.");
+    } else {
+      console.error(error);
+    }
+  });
