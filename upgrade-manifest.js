ec14053df4bab60d036e43994018154c468718c5
Daniel <daniel.s@crowdbotics.com>
2023-05-05 14:52:38 +0100

    fix lint issues

--- upgrade-manifest.js
+++ upgrade-manifest.js
@@ -1,3 +1,15 @@
+/**
+ * Instructions for generating this manifest for different destination versions.
+ *
+ * Start by generating the template targeting the destination version with
+ * cookiecutter, then generate the manifest with the `tree` command:
+ *
+ * $ pwd
+ * /repo/build/crowdbotics/build/v2/<slug>
+ *
+ * $ tree -iafF | grep -vE "\/$"
+ * # followed by a M-x replace-string RET <slug> RET {{slug}}
+ */
 export const manifest = [
   {
     old: "android/app/build.gradle",
@@ -20,8 +32,8 @@ export const manifest = [
     type: "text"
   },
   {
-    old: "android/app/src/debug/java/com/${slug}/ReactNativeFlipper.java",
-    new: "android/app/src/debug/java/com/${slug}/ReactNativeFlipper.java",
+    old: "android/app/src/debug/java/com/{{slug}}/ReactNativeFlipper.java",
+    new: "android/app/src/debug/java/com/{{slug}}/ReactNativeFlipper.java",
     type: "text"
   },
   {
@@ -30,13 +42,13 @@ export const manifest = [
     type: "text"
   },
   {
-    old: "android/app/src/main/java/com/${slug}/MainActivity.java",
-    new: "android/app/src/main/java/com/${slug}/MainActivity.java",
+    old: "android/app/src/main/java/com/{{slug}}/MainActivity.java",
+    new: "android/app/src/main/java/com/{{slug}}/MainActivity.java",
     type: "text"
   },
   {
-    old: "android/app/src/main/java/com/${slug}/MainApplication.java",
-    new: "android/app/src/main/java/com/${slug}/MainApplication.java",
+    old: "android/app/src/main/java/com/{{slug}}/MainApplication.java",
+    new: "android/app/src/main/java/com/{{slug}}/MainApplication.java",
     type: "text"
   },
   {
@@ -130,8 +142,8 @@ export const manifest = [
     type: "text"
   },
   {
-    old: "android/app/src/release/java/com/${slug}/ReactNativeFlipper.java",
-    new: "android/app/src/release/java/com/${slug}/ReactNativeFlipper.java",
+    old: "android/app/src/release/java/com/{{slug}}/ReactNativeFlipper.java",
+    new: "android/app/src/release/java/com/{{slug}}/ReactNativeFlipper.java",
     type: "addition"
   },
   { old: "android/build.gradle", new: "android/build.gradle", type: "text" },
@@ -390,133 +402,133 @@ export const manifest = [
   },
   { old: "ios/Podfile", new: "ios/Podfile", type: "text" },
   {
-    old: "ios/${slug}/AppDelegate.h",
-    new: "ios/${slug}/AppDelegate.h",
+    old: "ios/{{slug}}/AppDelegate.h",
+    new: "ios/{{slug}}/AppDelegate.h",
     type: "text"
   },
   {
-    old: "ios/${slug}/AppDelegate.m",
-    new: "ios/${slug}/AppDelegate.mm",
+    old: "ios/{{slug}}/AppDelegate.m",
+    new: "ios/{{slug}}/AppDelegate.mm",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Contents.json",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Contents.json",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Contents.json",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Contents.json",
     type: "json"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-20x20@1x.png",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-20x20@1x.png",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-20x20@1x.png",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-20x20@1x.png",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-20x20@2x.png",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-20x20@2x.png",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-20x20@2x.png",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-20x20@2x.png",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-20x20@3x.png",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-20x20@3x.png",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-20x20@3x.png",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-20x20@3x.png",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-29x29@1x.png",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-29x29@1x.png",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-29x29@1x.png",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-29x29@1x.png",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-29x29@3x.png",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-29x29@3x.png",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-29x29@3x.png",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-29x29@3x.png",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-40x40@1x.png",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-40x40@1x.png",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-40x40@1x.png",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-40x40@1x.png",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-40x40@2x.png",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-40x40@2x.png",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-40x40@2x.png",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-40x40@2x.png",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-40x40@3x.png",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-40x40@3x.png",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-40x40@3x.png",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-40x40@3x.png",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-60x60@2x.png",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-60x60@2x.png",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-60x60@2x.png",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-60x60@2x.png",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-60x60@3x.png",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-60x60@3x.png",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-60x60@3x.png",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-60x60@3x.png",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-76x76@1x.png",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-76x76@1x.png",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-76x76@1x.png",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-76x76@1x.png",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-76x76@2x.png",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-76x76@2x.png",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-76x76@2x.png",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-76x76@2x.png",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-83.5x83.5@2x.png",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-83.5x83.5@2x.png",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-83.5x83.5@2x.png",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/Icon-App-83.5x83.5@2x.png",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/AppIcon.appiconset/ItunesArtwork@2x.png",
-    new: "ios/${slug}/Images.xcassets/AppIcon.appiconset/ItunesArtwork@2x.png",
+    old: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/ItunesArtwork@2x.png",
+    new: "ios/{{slug}}/Images.xcassets/AppIcon.appiconset/ItunesArtwork@2x.png",
     type: "text"
   },
   {
-    old: "ios/${slug}/Images.xcassets/Contents.json",
-    new: "ios/${slug}/Images.xcassets/Contents.json",
+    old: "ios/{{slug}}/Images.xcassets/Contents.json",
+    new: "ios/{{slug}}/Images.xcassets/Contents.json",
     type: "json"
   },
   {
-    old: "ios/${slug}/Info.plist",
-    new: "ios/${slug}/Info.plist",
+    old: "ios/{{slug}}/Info.plist",
+    new: "ios/{{slug}}/Info.plist",
     type: "text"
   },
   {
-    old: "ios/${slug}/LaunchScreen.storyboard",
-    new: "ios/${slug}/LaunchScreen.storyboard",
+    old: "ios/{{slug}}/LaunchScreen.storyboard",
+    new: "ios/{{slug}}/LaunchScreen.storyboard",
     type: "text"
   },
   {
-    old: "ios/${slug}/main.m",
-    new: "ios/${slug}/main.m",
+    old: "ios/{{slug}}/main.m",
+    new: "ios/{{slug}}/main.m",
     type: "text"
   },
   {
-    old: "ios/${slug}Tests/Info.plist",
-    new: "ios/${slug}Tests/Info.plist",
+    old: "ios/{{slug}}Tests/Info.plist",
+    new: "ios/{{slug}}Tests/Info.plist",
     type: "text"
   },
   {
-    old: "ios/${slug}Tests/${slug}Tests.m",
-    new: "ios/${slug}Tests/${slug}Tests.m",
+    old: "ios/{{slug}}Tests/{{slug}}Tests.m",
+    new: "ios/{{slug}}Tests/{{slug}}Tests.m",
     type: "text"
   },
   {
-    old: "ios/${slug}.xcodeproj/project.pbxproj",
-    new: "ios/${slug}.xcodeproj/project.pbxproj",
+    old: "ios/{{slug}}.xcodeproj/project.pbxproj",
+    new: "ios/{{slug}}.xcodeproj/project.pbxproj",
     type: "text"
   },
   {
-    old: "ios/${slug}.xcodeproj/xcshareddata/xcschemes/${slug}.xcscheme",
-    new: "ios/${slug}.xcodeproj/xcshareddata/xcschemes/${slug}.xcscheme",
+    old: "ios/{{slug}}.xcodeproj/xcshareddata/xcschemes/{{slug}}.xcscheme",
+    new: "ios/{{slug}}.xcodeproj/xcshareddata/xcschemes/{{slug}}.xcscheme",
     type: "text"
   },
   { old: "ios/.xcode.env", new: "ios/.xcode.env", type: "addition" },
@@ -551,171 +563,10 @@ export const manifest = [
   { old: "tsconfig.json", new: "tsconfig.json", type: "addition" },
   { old: ".watchmanconfig", new: ".watchmanconfig", type: "text" },
   { old: "webpack.config.js", new: "webpack.config.js", type: "babel" },
-  { old: "yarn.lock", new: "yarn.lock", type: "text" }
+  { old: "yarn.lock", new: "yarn.lock", type: "text" },
+  {
+    old: ".crowdbotics.json",
+    new: ".crowdbotics.json",
+    type: "json"
+  }
 ];
-
-/*
- * disabled for now since it would stop the script entirely
- *
- * {
- *   old: path.join(".crowdbotics.json"),
- *   new: path.join(".crowdbotics.json"),
- *   type: "json"
- * }
- *
- * This manifest was generated with the help of the `tree` command:
- *
- * $ pwd
- * /repo/build/crowdbotics/build/v2/<slug>
- * $ tree -iafF | grep -vE "\/$"
- * # with a M-x replace-string RET <slug> RET ${slug}
- * .
- * ./android/app/build.gradle
- * ./android/app/debug.keystore
- * ./android/app/proguard-rules.pro
- * ./android/app/src/debug/AndroidManifest.xml
- * ./android/app/src/debug/java/com/${slug}/ReactNativeFlipper.java
- * ./android/app/src/main/AndroidManifest.xml
- * ./android/app/src/main/java/com/${slug}/MainActivity.java
- * ./android/app/src/main/java/com/${slug}/MainApplication.java
- * ./android/app/src/main/res/drawable/rn_edit_text_material.xml
- * ./android/app/src/main/res/mipmap-hdpi/ic_launcher_foreground.png
- * ./android/app/src/main/res/mipmap-hdpi/ic_launcher.png
- * ./android/app/src/main/res/mipmap-hdpi/ic_launcher_round.png
- * ./android/app/src/main/res/mipmap-mdpi/ic_launcher_foreground.png
- * ./android/app/src/main/res/mipmap-mdpi/ic_launcher.png
- * ./android/app/src/main/res/mipmap-mdpi/ic_launcher_round.png
- * ./android/app/src/main/res/mipmap-xhdpi/ic_launcher_foreground.png
- * ./android/app/src/main/res/mipmap-xhdpi/ic_launcher.png
- * ./android/app/src/main/res/mipmap-xhdpi/ic_launcher_round.png
- * ./android/app/src/main/res/mipmap-xxhdpi/ic_launcher_foreground.png
- * ./android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
- * ./android/app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png
- * ./android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_foreground.png
- * ./android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
- * ./android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png
- * ./android/app/src/main/res/values/strings.xml
- * ./android/app/src/main/res/values/styles.xml
- * ./android/app/src/release/java/com/${slug}/ReactNativeFlipper.java
- * ./android/build.gradle
- * ./android/fastlane/Appfile
- * ./android/fastlane/Fastfile
- * ./android/fastlane/metadata/android/en-US/images/phoneScreenshots/android-landscape.jpg
- * ./android/fastlane/metadata/android/en-US/images/phoneScreenshots/android-screenshot.jpg
- * ./android/fastlane/metadata/android/en-US/images/phoneScreenshots/wear-os.jpg
- * ./android/fastlane/metadata/android/en-US/title.txt
- * ./android/fastlane/Pluginfile
- * ./android/fastlane/README.md
- * ./android/gradle/wrapper/gradle-wrapper.jar
- * ./android/gradle/wrapper/gradle-wrapper.properties
- * ./android/gradle.properties
- * ./android/gradlew*
- * ./android/gradlew.bat
- * ./android/settings.gradle
- * ./app.json
- * ./App.tsx
- * ./babel.config.js
- * ./.bundle/config
- * ./.circleci/config.yml
- * ./.circleci/generate_mobile_android_config.sh
- * ./.circleci/generate_mobile_ios_config.sh
- * ./.circleci/webhook_callback.sh*
- * ./.crowdbotics.json
- * ./Dockerfile
- * ./.env.template
- * ./.eslintrc.js
- * ./Gemfile
- * ./Gemfile.lock
- * ./.github/.keep
- * ./.gitignore
- * ./heroku.yml
- * ./index.js
- * ./ios/fastlane/Appfile
- * ./ios/fastlane/Deliverfile
- * ./ios/fastlane/Fastfile
- * ./ios/fastlane/Matchfile
- * ./ios/fastlane/metadata/app_rating_config.json
- * ./ios/fastlane/metadata/copyright.txt
- * ./ios/fastlane/metadata/en-US/apple_tv_privacy_policy.txt
- * ./ios/fastlane/metadata/en-US/description.txt
- * ./ios/fastlane/metadata/en-US/keywords.txt
- * ./ios/fastlane/metadata/en-US/marketing_url.txt
- * ./ios/fastlane/metadata/en-US/name.txt
- * ./ios/fastlane/metadata/en-US/privacy_url.txt
- * ./ios/fastlane/metadata/en-US/promotional_text.txt
- * ./ios/fastlane/metadata/en-US/release_notes.txt
- * ./ios/fastlane/metadata/en-US/subtitle.txt
- * ./ios/fastlane/metadata/en-US/support_url.txt
- * ./ios/fastlane/metadata/primary_category.txt
- * ./ios/fastlane/metadata/primary_first_sub_category.txt
- * ./ios/fastlane/metadata/primary_second_sub_category.txt
- * ./ios/fastlane/metadata/review_information/review_demo_password.txt
- * ./ios/fastlane/metadata/review_information/review_demo_user.txt
- * ./ios/fastlane/metadata/review_information/review_email.txt
- * ./ios/fastlane/metadata/review_information/review_first_name.txt
- * ./ios/fastlane/metadata/review_information/review_last_name.txt
- * ./ios/fastlane/metadata/review_information/review_notes.txt
- * ./ios/fastlane/metadata/review_information/review_phone_number.txt
- * ./ios/fastlane/metadata/secondary_category.txt
- * ./ios/fastlane/metadata/secondary_first_sub_category.txt
- * ./ios/fastlane/metadata/secondary_second_sub_category.txt
- * ./ios/fastlane/README.md
- * ./ios/fastlane/screenshots/en-US/APP_IPHONE_55_LANDSCAPE.png
- * ./ios/fastlane/screenshots/en-US/APP_IPHONE_55_PORTRAIT.png
- * ./ios/fastlane/screenshots/en-US/APP_IPHONE_65_LANDSCAPE.png
- * ./ios/fastlane/screenshots/en-US/APP_IPHONE_65_PORTRAIT.png
- * ./ios/Podfile
- * ./ios/${slug}/AppDelegate.h
- * ./ios/${slug}/AppDelegate.mm
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/Contents.json
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-20x20@1x.png
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-20x20@2x.png
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-20x20@3x.png
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-29x29@1x.png
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-29x29@3x.png
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-40x40@1x.png
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-40x40@2x.png
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-40x40@3x.png
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-60x60@2x.png
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-60x60@3x.png
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-76x76@1x.png
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-76x76@2x.png
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/Icon-App-83.5x83.5@2x.png
- * ./ios/${slug}/Images.xcassets/AppIcon.appiconset/ItunesArtwork@2x.png
- * ./ios/${slug}/Images.xcassets/Contents.json
- * ./ios/${slug}/Info.plist
- * ./ios/${slug}/LaunchScreen.storyboard
- * ./ios/${slug}/main.m
- * ./ios/${slug}Tests/Info.plist
- * ./ios/${slug}Tests/${slug}Tests.m
- * ./ios/${slug}.xcodeproj/project.pbxproj
- * ./ios/${slug}.xcodeproj/xcshareddata/xcschemes/${slug}.xcscheme
- * ./ios/.xcode.env
- * ./metro.config.js
- * ./modules/index.js
- * ./modules/modules.js
- * ./modules/package.json
- * ./modules/yourapp.js
- * ./.node-version
- * ./options/index.js
- * ./options/options.js
- * ./package.json
- * ./.prettierrc.js
- * ./public/django_index.html
- * ./public/index.html
- * ./README.md
- * ./screens/index.js
- * ./screens/package.json
- * ./store/custom/.keep
- * ./store/index.js
- * ./store/package.json
- * ./store/README.md
- * ./__tests__/App-test.tsx
- * ./tsconfig.json
- * ./.watchmanconfig
- * ./webpack.config.js
- * ./yarn.lock
- *
- * 55 directories, 146 files
- */
