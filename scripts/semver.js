907e6cbcbf52e0ab631f735c96038b16a2c6866f
Daniel <daniel.s@crowdbotics.com>
2023-05-09 19:15:33 +0100

    introduce scaffold semver github action
    checks and enforces semver version bumps at:
    `scaffold/template/custom/.crowdbotics.json`
    
    this action only get's triggered when there's some change made to the
    following:
    - scaffold/**
    - dist/cookie/**

---
+++ scripts/semver.js
@@ -0,0 +1,42 @@
+import { spawnSync } from "node:child_process";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
+const __dirname = path.dirname(path.dirname(fileURLToPath(import.meta.url)));
+
+const { stdout } = spawnSync(
+  "git",
+  [
+    "diff",
+    "--cached",
+    "--unified=0",
+    "origin/master",
+    "--",
+    "scaffold/template/custom/.crowdbotics.json"
+  ],
+  {
+    cwd: __dirname,
+    encoding: "utf8"
+  }
+);
+
+console.log(stdout);
+
+if (!stdout.includes("version")) {
+  console.error(
+    "Expected a scaffold version change to be included in this PR."
+  );
+  process.exit(1);
+}
+
+const lines = stdout.split("\n");
+const numbers = lines
+  .filter((line) => line.includes("version"))
+  .map((line) => line.replace(/[^0-9]*/g, ""));
+
+if (numbers[1] < numbers[0]) {
+  console.error("Expected scaffold version to be higher in this PR.");
+  process.exit(1);
+} else if (numbers[1] > numbers[0]) {
+  console.log("Expected scaffold version change was found.");
+}
