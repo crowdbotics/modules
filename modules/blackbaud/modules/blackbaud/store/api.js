5d8d3f28fe30efca9215fa2d360052e77d30f8d7
Saad Bin Abid <saad.abid@crowdbotics.com>
2023-05-19 11:21:43 +0500

    update store

--- modules/blackbaud/modules/blackbaud/store/api.js
+++ modules/blackbaud/modules/blackbaud/store/api.js
@@ -262,6 +262,249 @@ export const getCountries = (accessToken) => {
   return axios.request(config);
 };
 
+// start
+export const getConstituentCustomFieldListSingleConstituent = (data) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${BASE_URL}/modules/blackbaud/constituents/get_constituent_custom_field_list_in_single_constituent/${data.id}/`,
+    headers: {
+      Authorization: `Bearer ${data.token}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getCustomFieldEducationList = (data) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${BASE_URL}/modules/blackbaud/constituents/get_custom_field_list_in_education/${data.id}/`,
+    headers: {
+      Authorization: `Bearer ${data.token}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEducationCustomFieldCategories = (accessToken) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${BASE_URL}/modules/blackbaud/constituents/get_education_custom_field_categories/`,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+
+export const getEducationDegrees = (accessToken) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${BASE_URL}/modules/blackbaud/constituents/get_education_degrees/`,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEducationConstituentList = (data) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/constituents/get_education_list_in_constituent/${data.id}/`,
+    headers: {
+      Authorization: `Bearer ${data.token}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEducationRecord = (data) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/constituents/get_education_record/${data.id}/`,
+    headers: {
+      Authorization: `Bearer ${data.token}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEducationsSchools = (accessToken) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/constituents/get_education_schools/`,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEducationStatuses = (accessToken) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/constituents/get_education_statuses/`,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEducationSubject = (accessToken) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/constituents/get_education_subjects/`,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEducationTypes = (accessToken) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/constituents/get_education_types/`,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEventAttachmentList = (data) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/event/${data.id}/get_event_attachment_list/`,
+    headers: {
+      Authorization: `Bearer ${data.token}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEventFees = (data) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/event/${data.id}/get_event_fees/`,
+    headers: {
+      Authorization: `Bearer ${data.token}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEventParticipantOptions = (data) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/event/${data.id}/get_event_participant_options/`,
+    headers: {
+      Authorization: `Bearer ${data.token}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEventParticipantsList = (data) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/event/participants_list/${data.id}/`,
+    headers: {
+      Authorization: `Bearer ${data.token}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEventParticipantsDonation = (data) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/event/${data.id}/get_event_participant_donation/`,
+    headers: {
+      Authorization: `Bearer ${data.token}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEventParticipantFeePayments = (data) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/event/${data.id}/get_event_participant_fee_payments/`,
+    headers: {
+      Authorization: `Bearer ${data.token}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEventParticipantFees = (data) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/event/${data.id}/get_event_participant_fees/`,
+    headers: {
+      Authorization: `Bearer ${data.token}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getParticipantOptions = (data) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/event/${data.id}/get_participant_options/`,
+    headers: {
+      Authorization: `Bearer ${data.token}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEventAttachment = (data) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/event/get_event_attachment/?event_id=${data.event_id}&attachment_id=${data.attachment_id}`,
+    headers: {
+      Authorization: `Bearer ${data.token}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+export const getEventAttachmentTags = (accessToken) => {
+  const config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${globalOptions.url}/modules/blackbaud/event/get_event_attachment_tags/`,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json"
+    }
+  };
+  return axios.request(config);
+};
+
 export const api = {
   getEventListing,
   getEventDetails,
@@ -282,5 +525,25 @@ export const api = {
   getConstituentDetails,
   getEducationList,
   getCurrencyConfiguration,
-  getCountries
+  getCountries,
+  getConstituentCustomFieldListSingleConstituent,
+  getCustomFieldEducationList,
+  getEducationCustomFieldCategories,
+  getEducationDegrees,
+  getEducationConstituentList,
+  getEducationRecord,
+  getEducationsSchools,
+  getEducationStatuses,
+  getEducationSubject,
+  getEducationTypes,
+  getEventAttachmentList,
+  getEventFees,
+  getEventParticipantOptions,
+  getEventParticipantsList,
+  getEventParticipantsDonation,
+  getEventParticipantFeePayments,
+  getEventParticipantFees,
+  getParticipantOptions,
+  getEventAttachment,
+  getEventAttachmentTags
 };
