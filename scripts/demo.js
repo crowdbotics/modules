39b2320da068054f49e3ced1141ad516313cbb4c
Daniel <daniel.s@crowdbotics.com>
2023-05-24 18:08:03 +0100

    make demo script use cookiecutter for both scaffolds
    no longer rely on `npx react-native init` which doesn't
    match what our backend does - which is to cookiecutter all scaffolds

--- scripts/demo.js
+++ scripts/demo.js
@@ -5,23 +5,23 @@ import config from "./config.js";
 import { generateCommand } from "./utils.js";
 import { execSync } from "child_process";
 
-const template = path.join("file:/", process.cwd(), config.scaffold.directory);
-
 const demoDir = path.join(process.cwd(), config.demo.directory);
 
 if (fs.existsSync(demoDir)) {
   fs.rmSync(demoDir, { recursive: true });
 }
 
-const command = generateCommand([
-  "npx react-native",
-  `init ${config.demo.placeholderName}`,
-  `--template ${template}`
+const rnCookieCutterCommand = generateCommand([
+  "pipenv run cookiecutter",
+  "./dist/cookie",
+  "--config-file cookiecutter.yaml",
+  "--no-input"
 ]);
 
-execSync(command);
+execSync(rnCookieCutterCommand, { encoding: "utf8", stdio: "inherit" });
+execSync("yarn install", { cwd: demoDir, enconding: "utf8", stdio: "inherit" });
 
-const cookiecutterCommand = generateCommand([
+const djangoCookieCutterCommand = generateCommand([
   "pipenv run cookiecutter",
   "gh:crowdbotics/django-scaffold",
   "--checkout master",
@@ -30,6 +30,5 @@ const cookiecutterCommand = generateCommand([
   "--no-input"
 ]);
 
-execSync(cookiecutterCommand);
-
+execSync(djangoCookieCutterCommand, { encoding: "utf8", stdio: "inherit" });
 fse.moveSync(path.join(demoDir, "demo"), path.join(demoDir, "backend"));
