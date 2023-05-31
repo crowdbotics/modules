1c7268ea8968aa834f05a5b37fb281325191c132
Daniel <daniel.s@crowdbotics.com>
2023-05-31 17:20:05 +0100

    add scaffold/package.json check in semver script

--- scripts/semver.js
+++ scripts/semver.js
@@ -1,4 +1,7 @@
+import fs from "node:fs";
+import path from "node:path";
 import { spawnSync } from "node:child_process";
+import { valid, invalid } from "../config.js";
 
 const __dirname = process.cwd();
 
@@ -18,23 +21,33 @@ const { stdout } = spawnSync(
   }
 );
 
-console.log(stdout);
+valid(".crowdbotics.json was found.", "\n", stdout);
 
 if (!stdout.includes("version")) {
-  console.error(
-    "Expected a scaffold version change to be included in this PR."
-  );
-  process.exit(1);
+  invalid("Expected a scaffold version change to be included in this PR.");
 }
+
+const onlyNumbersRegex = /[^0-9]*/g;
 
 const lines = stdout.split("\n");
 const numbers = lines
   .filter((line) => line.includes("version"))
-  .map((line) => line.replace(/[^0-9]*/g, ""));
+  .map((line) => line.replace(onlyNumbersRegex, ""));
 
 if (numbers[1] < numbers[0]) {
-  console.error("Expected scaffold version to be higher in this PR.");
-  process.exit(1);
+  invalid("Expected scaffold version to be higher in this PR.");
 } else if (numbers[1] > numbers[0]) {
-  console.log("Expected scaffold version change was found.");
+  valid("Expected scaffold version change was found.");
+  const templateVersion = JSON.parse(
+    fs.readFileSync(path.join("scaffold", "package.json"), "utf8")
+  ).version.replace(onlyNumbersRegex, "");
+  if (numbers[1] === templateVersion) {
+    valid(
+      "scaffold/package.json version matches scaffold/template/custom/.crowdbotics.json version."
+    );
+  } else {
+    invalid(
+      "Expected scaffold/package.json version to match scaffold/template/custom/.crowdbotics.json version."
+    );
+  }
 }
