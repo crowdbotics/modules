53ede8701d4a36268ef9985cf00be786002a543e
Daniel <daniel.s@crowdbotics.com>
2023-06-12 18:26:24 +0100

    yarn run bootstrap

--- dist/cookie/{{cookiecutter.project_slug}}/.circleci/generate_mobile_ios_config.sh
+++ dist/cookie/{{cookiecutter.project_slug}}/.circleci/generate_mobile_ios_config.sh
@@ -212,7 +212,7 @@ jobs:
 
       - run:
           name: Update CocaPods dependencies
-          command: bundle exec pod install --deployment --verbose
+          command: bundle exec pod install --verbose
           working_directory: ios
           timeout: 1200
 
