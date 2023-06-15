fa40f28dca14d3dc6cdf40572622fa31c2c48f94
Saad Bin Abid <saad.abid@crowdbotics.com>
2023-06-15 20:49:26 +0500

    Create OpenAi module, implement apis, create Readme

---
+++ modules/django-openai/django_openai/openai/README.md
@@ -0,0 +1,65 @@
+## Crowdbotics Django-openAI Module - Backend
+
+This module provides access to data and can also be used to generate and edit images or convert speech into text.
+
+
+## Scope Features
+The following are the key features in scope for this module.
+1. Ability to retrieve the list of models.
+2. Ability to retrieve the specific models.
+3. Ability to create chat completion.
+4. Ability to create completion.
+5. Ability to create edits.
+6. Ability to create image.
+7. Ability to create a transcription.
+8. Ability to create a translation.
+9. Ability to retrieve the list of engines.
+10. Ability to retrieve engines.
+
+
+
+## OpenAI Setup
+In order to configure the module, the first thing you should do is to create an app on Blackbaud. Here's the steps detailing that process:
+1. Sign up at openai account.
+2. On the top of the right corner, click on your profile, then on "on view API keys".
+3. After that create your API key by clicking on the "+ Create new secret key" button.
+4. Copy and save your API key because it generates one time only.
+
+
+## Installation
+1. In `.env` file add the following things:
+
+```py
+OPENAI_API_KEY=""
+
+```
+
+2. Run the following commands to get started:
+
+```
+python manage.py migrate
+python manage.py runserver
+```
+
+## Api Table
+List of api's endpoints with params needed for these apis.
+
+| Api Name                                                                       |                                      Params                                      | header                                     | Description                                                                   |
+|--------------------------------------------------------------------------------|:--------------------------------------------------------------------------------:|--------------------------------------------|-------------------------------------------------------------------------------|
+| `{GET}``/modules/openai/get_models_list/`                                      |                                        -`                                        | -                                          | This will return an list of models.`.                                         |
+| `{GET}``/modules/openai/get_model_detail/`                                     |                            `query_params: "model_id"`                            | -                                          | This will return the specific model details.                                  |                                                                                                                                      |
+| `{GET}``/modules/openai/get_engine_list/`                                      |                                        -                                         | -                                          | This will return an engines list containing all its objects.                  |
+| `{GET}``/modules/openai/get_engine_detail/`                                    |                                        -                                         | -                                          | This will return an engine detail.                                            |
+| `{POST}``/modules/openai/create_chat_completion/`                              |      `body_params: {"model": "", "messages": [{"role": "","content": ""}]}`      | -                                          | This will return an created chat response.                                    |
+| `{POST}``/modules/openai/create_a_completion/`                                 |    `body_params: {"model": "", "model": "", prompt": "","max_tokens": ""}]}`     | -                                          | This will return an created completion response.                              |
+| `{POST}``/modules/openai/create_an_edit/`                                      |            `body_params: {"model": "", "instruction":"", "input":""}`            | -                                          | This api will create edit for the provided input, instruction, and parameter. |
+| `{POST}``/modules/openai/create_an_image/`                                     |                 `body_params: {"prompt": "", "n":"", "size":""}`                 | -                                          | This api is used to return as your input image url.                           |
+| `{POST}``/modules/openai/create_an_transcription/`                             |                     `body_params: {"file": "", "model":""}`                      | -                                          | This will return an transcript text.                                          |
+| `{POST}``/modules/openai/create_an_translation/`                               |                 `body_params: {"file": "", "model":""}`                          | -                                          | This will return an translated text of your input.                            |
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ modules/django-openai/django_openai/openai/viewsets.py
@@ -0,0 +1,138 @@
+import os
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser, MultiPartParser
+from .services.OpenAIServices import OpenAIService
+from .serializers import CreateChatCompletionSerializer, CreateCompletionSerializer, CreateEditsSerializer, \
+    CreateImageSerializer, CreateTranscriptionSerializer, CreateTranslationSerializer
+
+
+class OpenAiViewSet(viewsets.GenericViewSet):
+    parser_classes = (JSONParser, MultiPartParser)
+    openai_service = OpenAIService(
+        base_url=os.getenv('OPENAI_BASE_URL', "https://api.openai.com"),
+        openai_api_key=os.getenv('OPENAI_API_KEY', ""),
+    )
+
+    allowed_serializer = {
+        "create_a_chat_completion": CreateChatCompletionSerializer,
+        "create_a_completion": CreateCompletionSerializer,
+        "create_an_edit": CreateEditsSerializer,
+        "create_an_image": CreateImageSerializer,
+        "create_an_transcription": CreateTranscriptionSerializer,
+        "create_an_translation": CreateTranslationSerializer
+    }
+
+    def get_serializer_class(self):
+        return self.allowed_serializer.get(self.action)
+
+    @action(detail=False, methods=['get'], url_path='get_models_list')
+    def get_models_list(self, request):
+        """
+        To get the models list
+        :return: Returns all list of models.
+        """
+        response = self.openai_service.list_models()
+        return Response(data=response.get("data"), status=response.get("status_code"))
+
+    @action(detail=False, methods=['get'], url_path='get_model_detail')
+    def get_model_detail(self, request, *args, **kwargs):
+        """
+        To get the models details
+        :query_params: model_id
+        :return: Returns models details.
+        """
+        response = self.openai_service.retrieve_model(model_id=self.request.query_params.get('model_id'))
+        return Response(data=response.get("data"), status=response.get("status_code"))
+
+    @action(detail=False, methods=['get'], url_path='get_engine_list')
+    def get_engine_list(self, request):
+        """
+        To get the engine list
+        :return: Returns list of engine.
+        """
+        response = self.openai_service.list_engine()
+        return Response(data=response.get("data"), status=response.get("status_code"))
+
+    @action(detail=False, methods=['get'], url_path='get_engine_detail')
+    def get_engine_detail(self, request, *args, **kwargs):
+        """
+        To get the engine details'
+        :query_params: engine_id
+        :return: Returns models details.
+        """
+        response = self.openai_service.retrieve_engine(engine_id=self.request.query_params.get('engine_id'))
+        return Response(data=response.get("data"), status=response.get("status_code"))
+
+    @action(detail=False, methods=['post'], url_path='create_chat_completion')
+    def create_a_chat_completion(self, request):
+        """
+        To create a chat completion
+        :payload: {"model": "", "messages": ["role": "", "content": ""]}
+        :return: Returns a newly created chat completion detail.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response = self.openai_service.create_chat_completion(payload=serializer.data)
+        return Response(data=response.get("data"), status=response.get("status_code"))
+
+    @action(detail=False, methods=['post'], url_path='create_a_completion')
+    def create_a_completion(self, request):
+        """
+        To create a completion
+        :payload: {"model": "", "prompt": "", "max_tokens": "", "temperature": ""}
+        :return: Returns a newly created completion.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response = self.openai_service.create_completion(payload=serializer.data)
+        return Response(data=response.get("data"), status=response.get("status_code"))
+
+    @action(detail=False, methods=['post'], url_path='create_an_edit')
+    def create_an_edit(self, request):
+        """
+        To create an edit
+        :payload: {"model": "", "input": "", "instruction": ""}
+        :return: Returns a newly created edit with detail.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response = self.openai_service.create_edit(payload=serializer.data)
+        return Response(data=response.get("data"), status=response.get("status_code"))
+
+    @action(detail=False, methods=['post'], url_path='create_an_image')
+    def create_an_image(self, request):
+        """
+        To create an edit
+        :payload: {"prompt": "", "n": int, "size": ""}
+        :return: Returns a newly created image url.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response = self.openai_service.create_image(payload=serializer.data)
+        return Response(data=response.get("data"), status=response.get("status_code"))
+
+    @action(detail=False, methods=['post'], url_path='create_an_transcription')
+    def create_an_transcription(self, request):
+        """
+        To create a transcription
+        :payload: {"file": "", "model": ""}
+        :return: Returns a newly created transcription text.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response = self.openai_service.create_transcription(payload=serializer.validated_data)
+        return Response(data=response.get("data"), status=response.get("status_code"))
+
+    @action(detail=False, methods=['post'], url_path='create_an_translation')
+    def create_an_translation(self, request):
+        """
+        To create a translation
+        :payload: {"file": "", "model": ""}
+        :return: Returns a newly created translation text.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response = self.openai_service.create_translation(payload=serializer.validated_data)
+        return Response(data=response.get("data"), status=response.get("status_code"))
