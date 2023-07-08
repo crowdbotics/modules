2a6d82d385e0a0480c4f2dca247e413c19439422
Jan Acuna <jan.acuna@crowdbotics.com>
2023-07-08 07:50:59 -0500

    PLAT-11769 adding welcome template styles inside the code file

--- scaffold/template/custom/screens/welcome/index.js
+++ scaffold/template/custom/screens/welcome/index.js
@@ -1,6 +1,13 @@
 import React from "react"
-import { View, Image, Text, ScrollView, SafeAreaView } from "react-native"
-import { styles } from "./styles"
+import {
+  View,
+  Image,
+  Text,
+  ScrollView,
+  SafeAreaView,
+  StyleSheet,
+  Platform
+} from "react-native"
 
 const WelcomeScreen = () => {
   return (
@@ -18,5 +25,57 @@ const WelcomeScreen = () => {
     </SafeAreaView>
   )
 }
+
+const containerMobile = {
+  backgroundColor: "#F8F8FC",
+  height: "100%"
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...Platform.select({
+      web: {
+        backgroundColor: "#F8F8FC",
+        minHeight: "100vh"
+      },
+      ios: {
+        ...containerMobile
+      },
+      android: {
+        ...containerMobile
+      }
+    })
+  },
+  scrollView: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 20
+  },
+  group: {
+    alignItems: "center"
+  },
+  logo: {
+    height: 180,
+    width: 180,
+    backgroundColor: "#ECEEF8",
+    padding: 40,
+    borderRadius: 30,
+    margin: 40
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 28,
+    color: "#828AB0",
+    fontWeight: 700
+  },
+  footer: {
+    textAlign: "center",
+    fontSize: 18,
+    color: "#828AB0",
+    fontWeight: 700,
+    marginBottom: 20
+  }
+})
 
 export default WelcomeScreen
