56d83c5f01189a81d715a42232aafd4223ec6688
Daniel Sousa <mail@danielsousa.io>
2023-08-23 19:12:06 +0100

    reformat package.json and index.js

--- modules/react-native-appointment-local/index.js
+++ modules/react-native-appointment-local/index.js
@@ -1,18 +1,9 @@
 import React from "react";
 import Navigator from "./Navigator";
-import { LogBox } from "react-native";
 import { slice } from "./store";
-
-const Appointment = () => {
-  LogBox.ignoreLogs(["VirtualizedLists should never be nested"]);
-
-  return (
-    <Navigator />
-  );
-};
 
 export default {
   title: "Appointment",
-  navigator: Appointment,
+  navigator: Navigator,
   slice: slice
 };
