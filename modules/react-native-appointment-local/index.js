d52afcea87b2ba234a1d2682e23a13e454b081c5
saad-abid-crowdbotics <76822297+saad-abid-crowdbotics@users.noreply.github.com>
2023-08-23 23:12:45 +0500

    React native appointment-local module (#397)
    * React native appointment-local
    
    * fixed global url
    
    * README.md updated
    
    * Added module features, refrence document and postman collection in readme file.
    
    * Update README.md
    
    * Implement store, add doc strings and optimize code for components
    
    * Add doc strings and optimize screen code
    
    * Code optimization
    
    * reformat README.md
    
    - replace all "actual intentions." strings with "features."
    
    * reformat package.json and index.js
    
    ---------
    
    Co-authored-by: Daniel Sousa <mail@danielsousa.io>

---
+++ modules/react-native-appointment-local/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Navigator from "./Navigator";
+import { slice } from "./store";
+
+export default {
+  title: "Appointment",
+  navigator: Navigator,
+  slice: slice
+};
