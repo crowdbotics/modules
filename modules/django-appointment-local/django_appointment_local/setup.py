b708423e5882d51043fcfc4a49c0063ca6385cca
Saad Bin Abid <saad.abid@crowdbotics.com>
2023-08-23 20:46:06 +0100

    fixed naming convention for root folder

--- modules/django-appointment-local/django_appointment_local/setup.py
+++ modules/django-appointment-local/django_appointment_local/setup.py
@@ -10,7 +10,7 @@ class BuildCommand(build):
 
 
 setup(
-    name="cb_appointment_local",
+    name="cb_django_appointment_local",
     version="0.1",
     packages=["appointment_local"],
     install_requires=[],
