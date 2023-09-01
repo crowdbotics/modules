09fadc322f538b3ff7d6864cadb81e8089b9d2ad
Saad bin abid <saad.abid@crowdbotics.com>
2023-09-02 01:13:28 +0500

    Lint ffixes

--- modules/react-native-zoom-free-calling/index.js
+++ modules/react-native-zoom-free-calling/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, Image, NativeEventEmitter, StyleSheet } from "react-native";
 
 import ZoomUs, { ZoomEmitter } from "react-native-zoom-us";
