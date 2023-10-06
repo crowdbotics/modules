a2cb4af35d5e44a5884db1352e69699a06cc2859
Daniel Sousa <mail@danielsousa.io>
2023-10-06 14:53:56 +0100

    expose commit command

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ import { parseModules } from "./scripts/parse.js";
 import { createDemo } from "./scripts/demo.js";
 import { addModules } from "./scripts/add.js";
 import { removeModules } from "./scripts/remove.js";
+import { commitModules } from "./scripts/commit-module.js";
 import { valid, invalid, section } from "./scripts/utils.js";
 
 const userdir = process.cwd();
@@ -45,6 +46,14 @@ const commands = {
       invalid("please provide the name of the modules to be removed");
     }
     removeModules(modules, "demo");
+  },
+  commit: () => {
+    const args = arg({});
+    const modules = args._.slice(1);
+    if (!modules.length) {
+      invalid("please provide the name of the modules to be commited");
+    }
+    commitModules(modules, "demo");
   },
   demo: () => {
     createDemo(
