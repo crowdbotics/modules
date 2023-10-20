39b74001cfc9ae8e12ee8895735020886d98b66e
Shoaib Amjad <muhammad.shoaib@crowdbotics.com>
2023-10-20 19:49:51 +0500

    Bug fixes

--- modules/firebase-push-notifications/backend/modules/firebase_push_notifications/firebase_push_notifications/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PushNotificationConfig(AppConfig):
-    name = "modules.firebase_push_notifications.firebase_push_notifications"
-    verbose_name = "Firebase Push Notification"
