0a96618a4be01cb40f8eeb54f18945c40407b05c
Shoaib Amjad <muhammad.shoaib@crowdbotics.com>
2023-10-25 17:04:26 +0500

    Updated setup.py for adding sentory import in settings.py

--- modules/django-monitoring/django_monitoring/setup.py
+++ modules/django-monitoring/django_monitoring/setup.py
@@ -1,5 +1,28 @@
+import os
+from pathlib import Path
+
 from setuptools import setup
 from setuptools.command.build import build
+
+try:
+    base_dir = Path.cwd()
+    settings_file_path = None
+    for root, dirs, files in os.walk(base_dir.parent.parent):
+        if 'settings.py' in files:
+            settings_file_path = f"{root}/settings.py"
+
+    import_statement = "from modules.django_monitoring.monitoring.sentry_configurations import *\n"
+
+    if settings_file_path:
+        with open(settings_file_path, 'r') as file:
+            lines = file.readlines()
+        index_to_insert = lines.index("from modules.manifest import get_modules\n")
+        if import_statement not in lines:
+            lines.insert(index_to_insert, import_statement)
+            with open(settings_file_path, 'w') as file:
+                file.write(''.join(lines))
+except (Exception,):
+    pass
 
 
 # Override build command
