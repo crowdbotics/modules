531a04d709b0315042babe05793fe4484e576042
saad-abid-crowdbotics <76822297+saad-abid-crowdbotics@users.noreply.github.com>
2023-10-31 17:29:07 +0500

    Update device registration endpoint (Bug Fixes) (#993)
    * Update device registeration endpoint
    
    * Lint Fixes

--- modules/firebase-push-notifications/modules/firebase-push-notifications/api.js
+++ modules/firebase-push-notifications/modules/firebase-push-notifications/api.js
@@ -1,12 +1,11 @@
 import { getGlobalOptions } from "@options";
-import { Platform } from "react-native";
 
 const global = getGlobalOptions();
 // Change your BASE_URL in `options/options.js` to edit this value
 const BASE_URL = global.url;
 
 export const registerDeviceInfoAPI = async (data, authToken) => {
-  const response = await fetch(`${BASE_URL}/modules/firebase-push-notifications/device/${Platform.OS === "ios" ? "apns" : "fcm"}/`, {
+  const response = await fetch(`${BASE_URL}/modules/firebase-push-notifications/user_fcm_device_add/`, {
     headers: {
       "Content-Type": "application/json",
       Authorization: "Token " + authToken
