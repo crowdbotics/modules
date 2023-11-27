1d3a1a5a28ad6d4aba9bdfc6550d0c46b57ada54
Daniel Sousa <mail@danielsousa.io>
2023-11-27 21:46:59 +0000

    new module

---
+++ modules/react-native-new/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function New() {
+  return (
+    <View>
+      <Text>new</Text>
+    </View>
+  );
+}
+
+export default {
+  title: "new",
+  navigator: New
+};
