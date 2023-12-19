f502be10b8b827b37ca493334f6fcafd308452dd
Taylor Dixon <taylor.dixon@crowdbotics.com>
2023-12-18 18:39:37 -0700

    PLAT-13080: Tidyup of publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -8,6 +8,73 @@ const POLL_INTERVAL = 2000;
 
 const IN_PROGRESS_STATUSES = ["PENDING", "STARTED"];
 
+const waitForTaskToComplete = async (taskId) => {
+  const waitingSpinner = ora(
+    "Waiting for publish to complete. This may take a few minutes.\n\n Publishing will continue if you exit here (CTRL+C)."
+  ).start();
+
+  let latestProcessingResultBody;
+
+  // Poll indefinitely until the task is resolved.
+  while (true) {
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, POLL_INTERVAL);
+    });
+
+    const processingResult = await apiClient.get({
+      path: "/v2/status-of-published-module/" + taskId
+    });
+
+    if (!processingResult.ok) {
+      waitingSpinner.stop();
+
+      // TODO - ask the BE to supply a 400 when there's a regular failure?
+      if (processingResult.status === 500) {
+        const result = await processingResult.text();
+        invalid(`
+Crowdbotics CLI has encountered an error while waiting for the publish command to complete. Publishing may still finish successfully, however you may contact support with reference id: '${taskId}'
+Error details: ${result}
+`);
+      }
+
+      invalid(
+        `Crowdbotics CLI has encountered an error while waiting for the publish command to complete. Publishing may still finish successfully, however you may contact support with reference id: '${taskId}'`
+      );
+      break;
+    }
+
+    latestProcessingResultBody = await processingResult.json();
+
+    // There were some instances while testing where status was coming back as undefined. This may not be an issue anymore.
+    if (!latestProcessingResultBody.status) {
+      waitingSpinner.stop();
+      invalid(
+        `Crowdbotics CLI has encountered an error while waiting for the publish command to complete. Publishing may still finish successfully, however you may contact support with reference id: '${taskId}'`
+      );
+    }
+
+    // If we've reached an end state for the polling, end the polling loop.
+    if (!IN_PROGRESS_STATUSES.includes(latestProcessingResultBody.status)) {
+      break;
+    }
+  }
+
+  waitingSpinner.stop();
+
+  if (latestProcessingResultBody.status === "FAILURE") {
+    invalid(`
+    Crowdbotics CLI has encountered an error while waiting for the publish command to complete. Publishing may still finish successfully, however you may contact support with reference id: '${taskId}'
+    Error details: ${JSON.stringify(latestProcessingResultBody)}
+    `);
+  }
+
+  if (latestProcessingResultBody.status === "SUCCESS") {
+    valid(`Publish successful. Reference id ${taskId}.`);
+  }
+};
+
 export const publish = async () => {
   section("Publishing modules.");
   section(
@@ -89,74 +156,6 @@ export const publish = async () => {
     invalid("Unable to publish module to catalog.");
   }
 
-  const waitingSpinner = ora(
-    "Waiting for publish to complete. This may take a few minutes.\n\n Publishing will continue if you exit here (CTRL+C)."
-  ).start();
-
   const taskResult = await createResult.json();
-
-  let latestProcessingResultBody;
-
-  // Poll indefinitely until the task is resolved.
-  while (true) {
-    await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, POLL_INTERVAL);
-    });
-
-    const processingResult = await apiClient.get({
-      path: "/v2/status-of-published-module/" + taskResult.task_id
-    });
-
-    if (!processingResult.ok) {
-      waitingSpinner.stop();
-
-      // TODO - ask the BE to supply a 400 when there's a regular failure?
-      if (processingResult.status === 500) {
-        const result = await processingResult.text();
-        invalid(`
-Crowdbotics CLI has encountered an error while waiting for the publish command to complete. Publishing may still finish successfully, however you may contact support with reference id: '${taskResult.task_id}'
-Error details: ${result}
-`);
-      }
-
-      invalid(
-        `Crowdbotics CLI has encountered an error while waiting for the publish command to complete. Publishing may still finish successfully, however you may contact support with reference id: '${taskResult.task_id}'`
-      );
-      break;
-    }
-
-    latestProcessingResultBody = await processingResult.json();
-
-    // There were some instances while testing where status was coming back as undefined. This may not be an issue anymore.
-    if (!latestProcessingResultBody.status) {
-      waitingSpinner.stop();
-      invalid(
-        `Crowdbotics CLI has encountered an error while waiting for the publish command to complete. Publishing may still finish successfully, however you may contact support with reference id: '${taskResult.task_id}'`
-      );
-    }
-
-    // If we've reached an end state for the polling, end the polling loop.
-    if (!IN_PROGRESS_STATUSES.includes(latestProcessingResultBody.status)) {
-      break;
-    }
-  }
-
-  waitingSpinner.stop();
-
-  if (latestProcessingResultBody.status === "FAILURE") {
-    invalid(`
-    Crowdbotics CLI has encountered an error while waiting for the publish command to complete. Publishing may still finish successfully, however you may contact support with reference id: '${
-      taskResult.task_id
-    }'
-    Error details: ${JSON.stringify(latestProcessingResultBody)}
-    `);
-  }
-
-  if (latestProcessingResultBody.status === "SUCCESS") {
-    valid(`Publish successful. Reference id ${taskResult.task_id}.`);
-  }
-
-  // TODO - print publish success message.
+  await waitForTaskToComplete(taskResult.task_id);
 };
