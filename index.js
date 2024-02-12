dfe370325c5c5b7fcafe1eef6950879696dbdc14
Robert So (robester0403) <85914248+robester0403@users.noreply.github.com>
2024-02-12 11:05:36 -0500

    PLAT-13621 added back amplitude

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ import { logout } from "./scripts/logout.js";
 import { modulesArchive, modulesGet, modulesList } from "./scripts/modules.js";
 import { publish } from "./scripts/publish.js";
 import { preExecuteChecks, preExecuteDjangoCheck } from "./scripts/utils/environment.js";
+import { Amplitude } from "./scripts/amplitude/wrapper.js";
 
 const pkg = JSON.parse(
   fs.readFileSync(new URL("package.json", import.meta.url), "utf8")
