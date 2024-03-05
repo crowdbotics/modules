7b438de218aeef5e7b809a4e7839bd7b84c90369
Taylor Dixon <taylor.dixon@crowdbotics.com>
2024-03-05 12:47:34 -0700

    Migrating environment check to new function

--- scripts/utils/environment.js
+++ scripts/utils/environment.js
@@ -9,6 +9,14 @@ const userdir = process.cwd();
 
 export const execOptions = { encoding: "utf8", stdio: "inherit" };
 
+export const EnvironmentDependency = {
+  Yarn: "yarn",
+  Git: "git",
+  Python: "python",
+  PipEnv: "pipenv",
+  CookieCutter: "cookiecutter"
+};
+
 /**
  * Setup Python environment to desired version
  * @param {*} options: exec options
@@ -17,59 +25,9 @@ export function configurePython(options = execOptions) {
   execSync("pipenv --python 3.8.17", options);
 }
 
-// TODO - Remove this
-export function preExecuteChecks(cookiecutterCheck = false) {
-  // Check if Python 3.8.x is installed
-
-  try {
-    section("Checking Python version");
-
-    const pythonVersionRegex = /Python 3\.[0-9]*/;
-
-    const pythonCheck = spawnSync("python", ["--version"], {
-      cwd: userdir,
-      shell: true,
-      encoding: "utf8"
-    });
-    const userPythonVersion = pythonCheck.stdout || pythonCheck.stderr;
-    if (!pythonVersionRegex.test(userPythonVersion)) {
-      invalid(
-        `Found Python version: ${userPythonVersion}. Please install 3.x and try again.`
-      );
-    }
-  } catch (error) {
-    invalid(
-      "Error detecting python version, please check install and try again."
-    );
-  }
-
-  if (cookiecutterCheck) {
-    // Check if Cookiecutter is installed
-    try {
-      execSync("cookiecutter --version", {
-        cwd: userdir,
-        shell: true,
-        encoding: "utf8"
-      });
-    } catch (error) {
-      invalid(
-        "Cookiecutter is not installed. Please install Cookiecutter before running this command."
-      );
-    }
-  }
-}
-
 function formatStdout(stdout) {
   return stdout.replace(/\n/g, "");
 }
-
-export const EnvironmentDependency = {
-  Yarn: "yarn",
-  Git: "git",
-  Python: "python",
-  PipEnv: "pipenv",
-  CookieCutter: "cookiecutter"
-};
 
 export function getEnvironmentVersions(dependencies) {
   const environmentVersions = {};
@@ -151,7 +109,6 @@ export function validateEnvironmentDependencies(
   ],
   force = false
 ) {
-  section("Scaffold upgrade started");
   section("Checking environment compatibility");
 
   const cachedEnvironmentVersions =
