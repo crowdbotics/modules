acd0dc7b9e54c94906ba52148e8c6b2ebcc04eac
Taylor Dixon <taylor.dixon@crowdbotics.com>
2024-03-26 15:32:48 -0600

    Updating help link

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ To migrate to the new version, please run the following commands:
   npm uninstall -g crowdbotics/modules
   npm i -g crowdbotics
 
-For more information, you can review our docs at docs.crowdbotics.com/<LINK PATH HERE>
+For more information, you can review our docs at https://docs.crowdbotics.com/docs/faq
 `);
