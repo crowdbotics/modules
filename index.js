e366609950d1e44f7dcca15ff261e75736ee3fc5
Taylor Dixon <taylor.dixon@crowdbotics.com>
2024-04-10 11:59:59 -0600

    PLAT-14318: Adding placeholder index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+console.log(`
+The Crowdbotics CLI has moved! 
+
+To migrate to the new version, please run the following commands:
+  npm uninstall -g crowdbotics/modules
+  npm i -g crowdbotics
+
+For more information, you can review our docs at https://docs.crowdbotics.com/docs/upgrade-cli
+`);
